d928bb732edd35002ce9087108464d54255c6497
cburns93 <cburns93@users.noreply.github.com>
2018-07-05 10:59:50 +0100

    Adds port checking

--- src/main/java/uk/gov/companieshouse/api/accounts/AccountsApplication.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/AccountsApplication.java
@@ -8,6 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class AccountsApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
+
+        // Returns the configured ${PORT} value in the `start.sh` script - passed in under `server.port`.
+        // If no port is configured, return null
+        Integer port = Integer.getInteger("server.port");
+
+        if (port == null) {
+            // When logging has been added, this print line will be replace
+            System.out.println("Failed to start service, no port has been configured");
+            System.exit(0);
+        }
+
         SpringApplication.run(AccountsApplication.class, args);
     }
 }
