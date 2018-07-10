263055ec8fc5bda3b4b96a70856e0ac0caf465cf
cburns93 <cburns93@users.noreply.github.com>
2018-07-10 13:58:41 +0100

    removes deprecated CustomConversions class

--- src/main/java/uk/gov/companieshouse/api/accounts/MongoConfig.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/MongoConfig.java
@@ -1,18 +1,13 @@
 package uk.gov.companieshouse.api.accounts;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.core.convert.DbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import uk.gov.companieshouse.logging.Logger;
-import uk.gov.companieshouse.logging.LoggerFactory;
 
 /**
  * _class maps to the model class in mongoDB (i.e. _class : uk.gov.companieshouse.Transaction)
@@ -24,23 +19,14 @@ import uk.gov.companieshouse.logging.LoggerFactory;
 @Configuration
 public class MongoConfig {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger("company-accounts.api.ch.gov.uk");
-
     @Bean
-    public MappingMongoConverter mappingMongoConverter(MongoDbFactory factory, MongoMappingContext context,
-            BeanFactory beanFactory) {
+    public MappingMongoConverter mappingMongoConverter(MongoDbFactory factory, MongoMappingContext context) {
         DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
         MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, context);
-        try {
-            mappingConverter.setCustomConversions(beanFactory.getBean(CustomConversions.class));
-        } catch (NoSuchBeanDefinitionException ignore) {
-            LOGGER.error("No such bean definition:", ignore);
-        }
 
         // Don't save _class to mongo
         mappingConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
 
         return mappingConverter;
     }
-
 }
