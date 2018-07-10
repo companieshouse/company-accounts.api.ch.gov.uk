66c389e2f79cd0c6669d89157a209904b1a7adca
cburns93 <cburns93@users.noreply.github.com>
2018-07-10 11:17:34 +0100

    puts data modelling in service class

--- src/main/java/uk/gov/companieshouse/api/accounts/service/impl/AccountServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/service/impl/AccountServiceImpl.java
@@ -1,8 +1,14 @@
 package uk.gov.companieshouse.api.accounts.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.GenerateEtagUtil;
+import uk.gov.companieshouse.api.accounts.Kind;
+import uk.gov.companieshouse.api.accounts.LinkType;
+import uk.gov.companieshouse.api.accounts.model.entity.AccountDataEntity;
 import uk.gov.companieshouse.api.accounts.model.entity.AccountEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.Account;
 import uk.gov.companieshouse.api.accounts.repository.AccountRepository;
@@ -17,14 +23,31 @@ public class AccountServiceImpl implements AccountService {
     /**
      * {@inheritDoc}
      */
+
     public Account createAccount(Account account) {
-        AccountEntity accountEntity = new AccountEntity().build(account);
+        AccountDataEntity accountDataEntity = new AccountDataEntity();
+        AccountEntity accountEntity = new AccountEntity();
+        BeanUtils.copyProperties(account, accountDataEntity);
+
+        //set properties etag, link
+        generateEtagLinksKind(accountDataEntity);
+        accountEntity.setData(accountDataEntity);
 
         AccountEntity createdAccount = accountRepository.insert(accountEntity);
 
-        account = new Account();
         BeanUtils.copyProperties(createdAccount.getData(), account);
 
         return account;
     }
+
+    private void generateEtagLinksKind(AccountDataEntity accountDataEntity) {
+        accountDataEntity.setEtag(GenerateEtagUtil.generateEtag());
+        accountDataEntity.setKind(Kind.ACCOUNT.getValue());
+
+        Map<String, String> links = new HashMap<>();
+
+        links.put(LinkType.SELF.getLink(), "");
+        accountDataEntity.setLinks(links);
+    }
+
 }
