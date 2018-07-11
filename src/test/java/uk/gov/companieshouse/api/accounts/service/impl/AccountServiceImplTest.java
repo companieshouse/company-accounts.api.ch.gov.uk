79ca34433e545f6bac91ae7cde5da04fe510792c
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-07-11 16:46:57 +0100

    adding service layer unit test

---
+++ src/test/java/uk/gov/companieshouse/api/accounts/service/impl/AccountServiceImplTest.java
@@ -0,0 +1,48 @@
+package uk.gov.companieshouse.api.accounts.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.accounts.model.entity.AccountEntity;
+import uk.gov.companieshouse.api.accounts.model.rest.Account;
+import uk.gov.companieshouse.api.accounts.repository.AccountRepository;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(Lifecycle.PER_CLASS)
+public class AccountServiceImplTest {
+
+    @Mock
+    private Account account;
+
+    @Mock
+    private AccountEntity createdAccountEntity;
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private AccountServiceImpl accountService;
+
+    @BeforeEach
+    public void setUp() {
+        when(accountRepository.insert(any(AccountEntity.class))).thenReturn(createdAccountEntity);
+    }
+
+    @Test
+    public void canCreateAccount() {
+        Account result = accountService.createAccount(account);
+        assertNotNull(result);
+        assertEquals(account, result);
+
+    }
+}
