e5103e85cad9e7ef156a2449d1e6c5a3ea5dcf5b
cburns93 <cburns93@users.noreply.github.com>
2018-07-19 10:30:06 +0100

    removes author

--- src/main/java/uk/gov/companieshouse/api/accounts/transaction/TransactionManager.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/transaction/TransactionManager.java
@@ -1,14 +1,7 @@
 package uk.gov.companieshouse.api.accounts.transaction;
 
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 
-/**
- * Interface to the Transaction API .
- *
- * @author dparameswaran
- *
- */
 public interface TransactionManager {
 
     ResponseEntity<Transaction> getTransaction(String id, String requestId);
