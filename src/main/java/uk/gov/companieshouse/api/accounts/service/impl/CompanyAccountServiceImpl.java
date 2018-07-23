60c8ee2a16be88482fd135a576a1f8fcab4af380
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-07-23 13:10:35 +0100

    removed commented out code

--- src/main/java/uk/gov/companieshouse/api/accounts/service/impl/CompanyAccountServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/service/impl/CompanyAccountServiceImpl.java
@@ -1,20 +1,18 @@
 package uk.gov.companieshouse.api.accounts.service.impl;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.accounts.Kind;
-import uk.gov.companieshouse.api.accounts.LinkType;
 import uk.gov.companieshouse.api.accounts.model.entity.CompanyAccountEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.CompanyAccount;
 import uk.gov.companieshouse.api.accounts.service.CompanyAccountService;
 import uk.gov.companieshouse.api.accounts.transformer.GenericTransformer;
 
 @Service
-public class CompanyAccountServiceImpl extends AbstractServiceImpl<CompanyAccount, CompanyAccountEntity> implements CompanyAccountService  {
+public class CompanyAccountServiceImpl extends
+        AbstractServiceImpl<CompanyAccount, CompanyAccountEntity> implements CompanyAccountService {
 
     @Autowired
     public CompanyAccountServiceImpl(
@@ -25,9 +23,7 @@ public class CompanyAccountServiceImpl extends AbstractServiceImpl<CompanyAccoun
 
     @Override
     public void addLinks(CompanyAccount rest) {
-//        Map<String, String> links = new HashMap<>();
-//        links.put(LinkType.SELF.getLink(), "");
-//        rest.setLinks(links);
+
     }
 
     @Override
