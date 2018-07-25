a17d84d8cba8a0918568f4d23d0e0e33ede98731
gpatrac <gpatra@companieshgouse.gov.uk>
2018-07-25 10:59:04 +0100

    moved the mongodb config beans from ApplicationConfig to MongoConfig

--- src/main/java/uk/gov/companieshouse/api/accounts/ApplicationConfiguration.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/ApplicationConfiguration.java
@@ -1,32 +1,16 @@
 package uk.gov.companieshouse.api.accounts;
 
-import com.mongodb.MongoClientOptions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
- * General application configuration .
+ * General application configuration.
+ *
+ * 1. Defines the application configurations
+ * 2. Enables AspectJ proxy for Spring AOP.
  */
 @Configuration
 @EnableAspectJAutoProxy
 public class ApplicationConfiguration {
 
-    @Autowired
-    private MongoDbConnectionPoolProperties configuration;
-
-    /**
-     * Create a {@link MongoClientOptions} .
-     *
-     * @return A {@link MongoClientOptions} .
-     */
-    @Bean
-    public MongoClientOptions mongoClientOptions() {
-        return MongoClientOptions.builder()
-                .minConnectionsPerHost(configuration.getMinSize())
-                .maxConnectionIdleTime(configuration.getMaxConnectionIdleTimeMS())
-                .maxConnectionLifeTime(configuration.getMaxConnectionLifeTimeMS())
-                .build();
-    }
 }
--- src/main/java/uk/gov/companieshouse/api/accounts/MongoDbConnectionPoolProperties.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/MongoDbConnectionPoolProperties.java
@@ -7,33 +7,37 @@ import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
 @Component
 public class MongoDbConnectionPoolProperties {
 
-    private Integer minSize;
-    private int maxConnectionIdleTimeMS;
-    private int maxConnectionLifeTimeMS;
+  private Integer minSize;
+  private int maxConnectionIdleTimeMS;
+  private int maxConnectionLifeTimeMS;
 
-    /**
-     * Constructs the config using environment variables for
-     * Mongo Connection Pool settings. Sets default values in case
-     * the environment variables are not supplied.
-     */
-    public MongoDbConnectionPoolProperties() {
-        EnvironmentReader reader = new EnvironmentReaderImpl();
+  /**
+   * Constructs the config using environment variables for Mongo Connection Pool settings. Sets
+   * default values in case the environment variables are not supplied.
+   */
+  public MongoDbConnectionPoolProperties() {
+    EnvironmentReader reader = new EnvironmentReaderImpl();
 
-        this.minSize = reader.getOptionalInteger("MONGO_CONNECTION_MAX_IDLE_KEY") != null ? this.minSize : 1;
+    this.minSize =
+        reader.getOptionalInteger("MONGO_CONNECTION_POOL_MIN_SIZE_KEY") != null ? this.minSize : 1;
 
-        this.maxConnectionIdleTimeMS = reader.getOptionalInteger("MONGO_CONNECTION_MAX_IDLE_KEY") != null ? this.maxConnectionIdleTimeMS : 0;
-        this.maxConnectionLifeTimeMS = reader.getOptionalInteger("MONGO_CONNECTION_MAX_LIFE_KEY") != null ? this.maxConnectionLifeTimeMS : 0;
-    }
+    this.maxConnectionIdleTimeMS =
+        reader.getOptionalInteger("MONGO_CONNECTION_MAX_IDLE_KEY") != null
+            ? this.maxConnectionIdleTimeMS : 0;
+    this.maxConnectionLifeTimeMS =
+        reader.getOptionalInteger("MONGO_CONNECTION_MAX_LIFE_KEY") != null
+            ? this.maxConnectionLifeTimeMS : 0;
+  }
 
-    public int getMinSize() {
-        return minSize;
-    }
+  public int getMinSize() {
+    return minSize;
+  }
 
-    public int getMaxConnectionIdleTimeMS() {
-        return maxConnectionIdleTimeMS;
-    }
+  public int getMaxConnectionIdleTimeMS() {
+    return maxConnectionIdleTimeMS;
+  }
 
-    public int getMaxConnectionLifeTimeMS() {
-        return maxConnectionLifeTimeMS;
-    }
+  public int getMaxConnectionLifeTimeMS() {
+    return maxConnectionLifeTimeMS;
+  }
 }
