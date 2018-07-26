fb1d980263c1bbfe47fe23ef3598e6b559052d67
cburns93 <cburns93@users.noreply.github.com>
2018-07-26 12:04:13 +0100

    minor refactor of interface
    - removes unneeded service annotation
    - removes redudant public access modifiers

--- src/main/java/uk/gov/companieshouse/api/accounts/service/AbstractService.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/service/AbstractService.java
@@ -1,20 +1,18 @@
 package uk.gov.companieshouse.api.accounts.service;
 
-import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.accounts.model.entity.BaseEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.RestObject;
 
-@Service
 public interface AbstractService<C extends RestObject, E extends BaseEntity> {
 
-    public C save(C rest);
+    C save(C rest);
 
-    public void addEtag(C rest);
+    void addEtag(C rest);
 
-    public void addLinks(C rest);
+    void addLinks(C rest);
 
-    public void addKind(C rest);
+    void addKind(C rest);
 
-    public void addID(E entity);
+    void addID(E entity);
 
 }
