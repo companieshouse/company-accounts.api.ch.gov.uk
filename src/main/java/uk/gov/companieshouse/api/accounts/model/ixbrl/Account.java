7b63bf9adcbc5f927a42cfe429fc02dad20f4816
Marco Cabreara <mcabrerahernandez@companieshouse.gov.uk>
2018-08-01 07:46:09 +0100

    Updating models and Adding new model to the ixbrl package

--- src/main/java/uk/gov/companieshouse/api/accounts/model/ixbrl/Account.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/ixbrl/Account.java
@@ -1,14 +1,18 @@
 package uk.gov.companieshouse.api.accounts.model.ixbrl;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import uk.gov.companieshouse.api.accounts.model.ixbrl.balancesheet.BalanceSheet;
+import uk.gov.companieshouse.api.accounts.model.ixbrl.company.Company;
 import uk.gov.companieshouse.api.accounts.model.ixbrl.notes.Notes;
 import uk.gov.companieshouse.api.accounts.model.ixbrl.period.Period;
 
 public class Account {
 
     private Period period;
+    @JsonProperty("balance_sheet")
     private BalanceSheet balanceSheet;
     private Notes notes;
+    private Company company;
 
     public Period getPeriod() {
         return period;
@@ -32,5 +36,13 @@ public class Account {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
     }
 }
