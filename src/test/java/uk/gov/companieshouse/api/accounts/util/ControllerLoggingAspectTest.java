b6838f14f6efe43dfea9ca29f1cb01e5fffd0ab4
gpatrac <gpatra@companieshgouse.gov.uk>
2018-08-06 16:16:41 +0100

    added tests to assert response failures

--- src/test/java/uk/gov/companieshouse/api/accounts/util/ControllerLoggingAspectTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/util/ControllerLoggingAspectTest.java
@@ -1,13 +1,18 @@
 package uk.gov.companieshouse.api.accounts.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.companieshouse.api.accounts.service.response.ResponseStatus.DUPLICATE_KEY_ERROR;
+import static uk.gov.companieshouse.api.accounts.service.response.ResponseStatus.SUCCESS;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -27,6 +32,8 @@ import uk.gov.companieshouse.api.accounts.AttributeName;
 import uk.gov.companieshouse.api.accounts.controller.CompanyAccountController;
 import uk.gov.companieshouse.api.accounts.model.rest.CompanyAccount;
 import uk.gov.companieshouse.api.accounts.service.CompanyAccountService;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseStatus;
 import uk.gov.companieshouse.api.accounts.transaction.Transaction;
 import uk.gov.companieshouse.api.accounts.util.logging.ControllerLoggingAspect;
 
@@ -51,7 +58,7 @@ public class ControllerLoggingAspectTest {
   @Mock
   private CompanyAccount companyAccount;
   @Mock
-  private CompanyAccount createdCompanyAccount;
+  private ResponseObject createdCompanyAccount;
   @Mock
   private Transaction transaction;
   @Mock
@@ -92,24 +99,59 @@ public class ControllerLoggingAspectTest {
   }
 
   @Test
-  public void controllerAspectIsTriggered() throws Throwable {
-    when(companyAccountService.save(any(CompanyAccount.class), anyString()))
-        .thenReturn(createdCompanyAccount);
-    when(request.getSession()).thenReturn(session);
-    when(session.getAttribute(AttributeName.TRANSACTION.getValue())).thenReturn(transaction);
-    when(transaction.getCompanyNumber()).thenReturn(COMPANY_ACCOUNT_ID);
-    Object response = proxy.createCompanyAccount(request, "transaction_id", companyAccount);
-    verify(companyAccountService, times(1)).save(companyAccount, COMPANY_ACCOUNT_ID);
-    verifyCompanyAccountResponse(response);
+  public void aspectTriggeredWhenResponseSuccessful() throws Throwable {
+    stubApiResponse(SUCCESS);
+    Object response = proxy.createCompanyAccount(companyAccount, request);
+    verify(companyAccountService, times(1)).createCompanyAccount(companyAccount, transaction,
+       "1234567890GB");
+    verifySuccessResponse(response);
   }
 
-  private void verifyCompanyAccountResponse(Object response) {
+  @Test
+  public void aspectTriggeredWhenFailed() throws Throwable {
+    stubApiResponse(DUPLICATE_KEY_ERROR);
+    Object response = proxy.createCompanyAccount(companyAccount, request);
+    verify(companyAccountService, times(1)).createCompanyAccount(companyAccount, transaction,
+        "1234567890GB");
+    verifyFailureResponse(response);
+  }
+
+  private void stubApiResponse(ResponseStatus status){
+    when(companyAccountService
+        .createCompanyAccount(companyAccount, transaction, "1234567890GB")).thenReturn(createdCompanyAccount);
+
+    when(request.getSession()).thenReturn(session);
+    when(request.getHeader("X-Request-Id")).thenReturn("1234567890GB");
+    when(session.getAttribute(AttributeName.TRANSACTION.getValue())).thenReturn(transaction);
+    stubResponseAndStatus(status);
+  }
+
+  private void stubResponseAndStatus(ResponseStatus status) {
+    if (status.equals(SUCCESS)) {
+      when(createdCompanyAccount.getStatus()).thenReturn(SUCCESS);
+      when(createdCompanyAccount.getData()).thenReturn(new Object());
+      return;
+    }
+    else if (status.equals(DUPLICATE_KEY_ERROR)){
+      when(createdCompanyAccount.getStatus()).thenReturn(DUPLICATE_KEY_ERROR);
+    }
+  }
+
+  private void verifySuccessResponse(Object response) {
     assertNotNull(response);
     assertTrue(response instanceof ResponseEntity);
     ResponseEntity responseEntity = (ResponseEntity) response;
     assertTrue(((ResponseEntity) response).getStatusCode().equals(HttpStatus.CREATED));
     assertNotNull(responseEntity.getBody());
     assertNotNull(responseEntity.getBody() instanceof CompanyAccount);
+  }
+
+  private void verifyFailureResponse(Object response) {
+    assertNotNull(response);
+    assertTrue(response instanceof ResponseEntity);
+    ResponseEntity responseEntity = (ResponseEntity) response;
+    assertTrue(((ResponseEntity) response).getStatusCode().equals(HttpStatus.CONFLICT));
+    assertNull(responseEntity.getBody());
   }
 
 
