03d66b0d0a5b7680f4813296563bdba947ca2682
gpatrac <gpatra@companieshgouse.gov.uk>
2018-08-10 12:29:31 +0100

    initial commit

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/interceptor/LoggingInterceptor.java
@@ -0,0 +1,21 @@
+package uk.gov.companieshouse.api.accounts.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+        ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+}
