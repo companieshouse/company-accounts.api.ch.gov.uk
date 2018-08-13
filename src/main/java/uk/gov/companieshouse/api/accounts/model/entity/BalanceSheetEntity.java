cbbb1229f0d145927d89debeceed71ae6733729f
gpatrac <gpatra@companieshgouse.gov.uk>
2018-08-13 11:28:51 +0100

    removed unwanted changes

--- src/main/java/uk/gov/companieshouse/api/accounts/model/entity/BalanceSheetEntity.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/entity/BalanceSheetEntity.java
@@ -1,10 +1,9 @@
 package uk.gov.companieshouse.api.accounts.model.entity;
 
-import java.io.Serializable;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-public class BalanceSheetEntity implements Serializable {
+public class BalanceSheetEntity {
 
     @NotNull
     @Field("called_up_share_capital_not_paid")
