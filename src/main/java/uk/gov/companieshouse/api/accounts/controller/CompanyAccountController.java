f9d71020800bce5863a6d5996ba95f51f3b85db1
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-08-16 16:22:42 +0100

    added 404 status return when objects are not in the request

--- src/main/java/uk/gov/companieshouse/api/accounts/controller/CompanyAccountController.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/controller/CompanyAccountController.java
@@ -1,6 +1,9 @@
 package uk.gov.companieshouse.api.accounts.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,18 +12,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.api.accounts.AttributeName;
+import uk.gov.companieshouse.api.accounts.CompanyAccountsApplication;
 import uk.gov.companieshouse.api.accounts.model.entity.CompanyAccountEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.CompanyAccount;
 import uk.gov.companieshouse.api.accounts.service.CompanyAccountService;
 import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
 import uk.gov.companieshouse.api.accounts.transaction.Transaction;
-import uk.gov.companieshouse.api.accounts.transformer.CompanyAccountTransformer;
 import uk.gov.companieshouse.api.accounts.utility.ApiResponseMapper;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 @RestController
+@RequestMapping(value = "/transactions/{transactionId}/company-accounts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CompanyAccountController {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(CompanyAccountsApplication.APPLICATION_NAME_SPACE);
 
     @Autowired
     private CompanyAccountService companyAccountService;
@@ -28,30 +38,36 @@ public class CompanyAccountController {
     @Autowired
     private ApiResponseMapper apiResponseMapper;
 
-    @Autowired
-    private CompanyAccountTransformer companyAccountTransformer;
-
-    @PostMapping(value = "/transactions/{transactionId}/company-accounts",
-        consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     public ResponseEntity createCompanyAccount(@Valid @RequestBody CompanyAccount companyAccount,
-        HttpServletRequest request) {
+            HttpServletRequest request) {
 
-        Transaction transaction = (Transaction) request.getAttribute(AttributeName.TRANSACTION.getValue());
+        Transaction transaction = (Transaction) request
+                .getAttribute(AttributeName.TRANSACTION.getValue());
 
         String requestId = request.getHeader("X-Request-Id");
-        ResponseObject result = companyAccountService.createCompanyAccount(companyAccount, transaction, requestId);
-return apiResponseMapper
-        .map(result.getStatus(), result.getData(), result.getErrorData());
+        ResponseObject result = companyAccountService
+                .createCompanyAccount(companyAccount, transaction, requestId);
+        return apiResponseMapper
+                .map(result.getStatus(), result.getData(), result.getErrorData());
     }
 
-    @GetMapping(value = "/transactions/{transactionId}/company-accounts/{companyAccountId}",
-        produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public ResponseEntity getCompanyAccount(HttpServletRequest request) {
 
-        CompanyAccountEntity companyAccountEntity = (CompanyAccountEntity) request.getSession()
-            .getAttribute(AttributeName.COMPANY_ACCOUNT.getValue());
-        CompanyAccount companyAccount = companyAccountTransformer.transform(companyAccountEntity);
-        return ResponseEntity.status(HttpStatus.OK).body(companyAccount);
+        final Map<String, Object> debugMap = new HashMap<>();
+        debugMap.put("request_method", request.getMethod());
+
+        CompanyAccountEntity companyAccountEntity = (CompanyAccountEntity) request
+                .getAttribute(AttributeName.COMPANY_ACCOUNT.getValue());
+        if (companyAccountEntity == null) {
+            debugMap.put("message",
+                    "CompanyAccountController error: No company account in request");
+            LOGGER.errorRequest(request, null, debugMap);
+            return ResponseEntity.status(HttpServletResponse.SC_NOT_FOUND).body(null);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(companyAccountEntity.getData());
 
     }
 }
--- src/main/java/uk/gov/companieshouse/api/accounts/controller/SmallFullController.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/controller/SmallFullController.java
@@ -1,6 +1,9 @@
 package uk.gov.companieshouse.api.accounts.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,17 +12,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.api.accounts.AttributeName;
+import uk.gov.companieshouse.api.accounts.CompanyAccountsApplication;
 import uk.gov.companieshouse.api.accounts.model.entity.SmallFullEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.SmallFull;
 import uk.gov.companieshouse.api.accounts.service.SmallFullService;
 import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
 import uk.gov.companieshouse.api.accounts.transaction.Transaction;
 import uk.gov.companieshouse.api.accounts.utility.ApiResponseMapper;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 @RestController
+@RequestMapping(value = "/transactions/{transactionId}/company-accounts/{companyAccountId}/small-full",
+        produces = MediaType.APPLICATION_JSON_VALUE)
 public class SmallFullController {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(CompanyAccountsApplication.APPLICATION_NAME_SPACE);
 
     @Autowired
     private SmallFullService smallFullService;
@@ -27,8 +39,7 @@ public class SmallFullController {
     @Autowired
     private ApiResponseMapper apiResponseMapper;
 
-    @PostMapping(value = "/transactions/{transactionId}/company-accounts/{companyAccountId}/small-full",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     public ResponseEntity create(@Valid @RequestBody SmallFull smallFull,
             HttpServletRequest request) {
         Transaction transaction = (Transaction) request
@@ -39,12 +50,20 @@ public class SmallFullController {
         return apiResponseMapper.map(result.getStatus(), result.getData(), result.getErrorData());
     }
 
-    @GetMapping(value = "/transactions/{transactionId}/company-accounts/{companyAccountId}/small-full",
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getCompanyAccount(HttpServletRequest request) {
+    @GetMapping
+    public ResponseEntity get(HttpServletRequest request) {
+
+        final Map<String, Object> debugMap = new HashMap<>();
+        debugMap.put("request_method", request.getMethod());
 
         SmallFullEntity smallFullEntity = (SmallFullEntity) request
-            .getAttribute(AttributeName.SMALLFULL.getValue());
+                .getAttribute(AttributeName.SMALLFULL.getValue());
+        if (smallFullEntity == null) {
+            debugMap.put("message",
+                    "CompanyAccountController error: No small-full account in request");
+            LOGGER.errorRequest(request, null, debugMap);
+            return ResponseEntity.status(HttpServletResponse.SC_NOT_FOUND).body(null);
+        }
 
         return ResponseEntity.status(HttpStatus.OK).body(smallFullEntity.getData());
     }
