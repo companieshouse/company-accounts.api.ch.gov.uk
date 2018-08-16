965cb3574275480e85be1acd975feae76f3b364b
gpatrac <gpatra@companieshgouse.gov.uk>
2018-08-16 11:06:13 +0100

    removed the unwanted throws clause

--- src/test/java/uk/gov/companieshouse/api/accounts/service/impl/SmallFullServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/service/impl/SmallFullServiceImplTest.java
@@ -54,7 +54,7 @@ public class SmallFullServiceImplTest {
 
         @Test
         @DisplayName("Tests the successful creation of a smallFull resource")
-        public void canCreateAccount() throws NoSuchAlgorithmException {
+        public void canCreateAccount() {
             ResponseObject<SmallFull> result = smallFullService.save(smallFull, "");
             assertNotNull(result);
             assertEquals(smallFull, result.getData());
