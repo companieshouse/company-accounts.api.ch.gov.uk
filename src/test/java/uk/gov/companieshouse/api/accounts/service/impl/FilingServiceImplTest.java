eb30cf196c667f9231442bd5d8eed56e59c24799
ch-mcabrera <mcabrerahernandez@companieshouse.gov.uk>
2018-08-30 14:47:21 +0100

    Adding junit test for implementation

---
+++ src/test/java/uk/gov/companieshouse/api/accounts/service/impl/FilingServiceImplTest.java
@@ -0,0 +1,27 @@
+package uk.gov.companieshouse.api.accounts.service.impl;
+
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import uk.gov.companieshouse.api.accounts.model.filing.Filing;
+import uk.gov.companieshouse.api.accounts.service.FilingService;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class FilingServiceImplTest {
+
+    private static final String TRANSACTION_ID = "1234561-1234561-1234561";
+    private static final String ACCOUNTS_ID = "1234561";
+    private FilingService filingService = new FilingServiceImpl();
+
+    @Test
+    @DisplayName("Tests the filing generation. Happy path")
+    void shouldGenerateFiling() {
+
+        Filing filing = filingService.generateAccountFiling(TRANSACTION_ID, ACCOUNTS_ID);
+        assertNull(filing);
+    }
+}
