94a7b72953b7a7d621a61042cdbffc8163f7c2a7
ch-mcabrera <mcabrerahernandez@companieshouse.gov.uk>
2018-08-30 14:51:07 +0100

    Updating  @params from comment

--- src/main/java/uk/gov/companieshouse/api/accounts/service/FilingService.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/service/FilingService.java
@@ -1,6 +1,5 @@
 package uk.gov.companieshouse.api.accounts.service;
 
-import java.io.IOException;
 import uk.gov.companieshouse.api.accounts.model.filing.Filing;
 
 public interface FilingService {
@@ -8,10 +7,9 @@ public interface FilingService {
     /**
      * Generate a filing with the ixbrl location that is generated for transaction and accounts id.
      *
-     * @param transaction - transaction
-     * @param accountEntity - accounts information
-     * @return {@link Filing}
-     * @throws IOException
+     * @param transaction
+     * @param accountEntity
+     * @return
      */
     Filing generateAccountFiling(String transaction, String accountEntity);
 }
