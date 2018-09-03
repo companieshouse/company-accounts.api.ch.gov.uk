c7ff3ca3a15e6c32ce1f3b909dcb72e813c411f2
ch-mcabrera <mcabrerahernandez@companieshouse.gov.uk>
2018-09-03 16:01:09 +0100

    Adding basic Filing Generator implementation Code changes to generate a Filing when it is a small full account.

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/utility/ixbrl/DocumentGeneratorCaller.java
@@ -0,0 +1,17 @@
+package uk.gov.companieshouse.api.accounts.utility.ixbrl;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Class to call the document generator to obtain the ixbrl location.
+ * Since the functionality has not been implemented yet, (STORY SFA-574), it returns an empty string.
+ *
+ * This class will change to call the new end point and it will return the s3 ixbrl location.
+ */
+@Component
+public class DocumentGeneratorCaller {
+
+    public String generateIxbrl() {
+        return "";
+    }
+}
