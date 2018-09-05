dbc0a2a13ca0f7ddabd9ae2e7e0b07bb61321280
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-09-05 20:56:14 +0100

    organise imports

--- src/main/java/uk/gov/companieshouse/api/accounts/controller/SmallFullController.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/controller/SmallFullController.java
@@ -5,13 +5,10 @@ import static uk.gov.companieshouse.api.accounts.CompanyAccountsApplication.APPL
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.api.accounts.AttributeName;
 import uk.gov.companieshouse.api.accounts.exception.DataException;
 import uk.gov.companieshouse.api.accounts.model.entity.CompanyAccountEntity;
-import uk.gov.companieshouse.api.accounts.model.entity.SmallFullEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.SmallFull;
 import uk.gov.companieshouse.api.accounts.service.SmallFullService;
 import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
@@ -29,8 +25,6 @@ import uk.gov.companieshouse.api.accounts.transformer.SmallFullTransformer;
 import uk.gov.companieshouse.api.accounts.utility.ApiResponseMapper;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
-import uk.gov.companieshouse.logging.util.LogContext;
-import uk.gov.companieshouse.logging.util.LogHelper;
 
 @RestController
 @RequestMapping(value = "/transactions/{transactionId}/company-accounts/{companyAccountId}/small-full",
@@ -51,7 +45,6 @@ public class SmallFullController {
     @PostMapping
     public ResponseEntity create(@Valid @RequestBody SmallFull smallFull,
             HttpServletRequest request) {
-
         Transaction transaction = (Transaction) request
                 .getAttribute(AttributeName.TRANSACTION.getValue());
         CompanyAccountEntity companyAccountEntity = (CompanyAccountEntity) request
