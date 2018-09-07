6a3dae70185bb0f0c8f6d4111ad01a86b5e14f9e
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-09-07 12:10:58 +0100

    increasing unit test coverage

--- src/test/java/uk/gov/companieshouse/api/accounts/service/impl/CurrentPeriodServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/service/impl/CurrentPeriodServiceImplTest.java
@@ -2,25 +2,38 @@ package uk.gov.companieshouse.api.accounts.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.mongodb.DuplicateKeyException;
+import com.mongodb.MongoException;
 import java.security.MessageDigest;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.api.accounts.exception.DataException;
 import uk.gov.companieshouse.api.accounts.model.entity.CurrentPeriodEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.CurrentPeriod;
+import uk.gov.companieshouse.api.accounts.model.rest.SmallFull;
 import uk.gov.companieshouse.api.accounts.repository.CurrentPeriodRepository;
 import uk.gov.companieshouse.api.accounts.service.SmallFullService;
 import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseStatus;
 import uk.gov.companieshouse.api.accounts.transaction.Transaction;
 import uk.gov.companieshouse.api.accounts.transformer.CurrentPeriodTransformer;
 
@@ -50,11 +63,16 @@ public class CurrentPeriodServiceImplTest {
     @Mock
     private CurrentPeriodTransformer currentPeriodTransformer;
 
+    @Mock
+    private DuplicateKeyException duplicateKeyException;
+
+    @Mock
+    private MongoException mongoException;
+
     @InjectMocks
     private CurrentPeriodServiceImpl currentPeriodService;
 
-    @BeforeEach
-    public void setUp() {
+    public void setUpCreate() {
         currentPeriodService.setMessageDigest(messageDigest);
         byte[] b = {12, 10, 56, 120, 13, 15};
         when(messageDigest.digest(any())).thenReturn(b);
@@ -63,11 +81,54 @@ public class CurrentPeriodServiceImplTest {
     @Test
     @DisplayName("Tests the successful creation of a currentPeriod resource")
     public void canCreateCurrentPeriod() throws DataException {
+        setUpCreate();
         when(currentPeriodTransformer.transform(currentPeriod)).thenReturn(currentPeriodEntity);
         ResponseObject<CurrentPeriod> result = currentPeriodService
                 .create(currentPeriod, transaction, "", "");
         assertNotNull(result);
         assertEquals(currentPeriod, result.getData());
+    }
 
+    @Test
+    @DisplayName("Tests the duplicate key when creating a current period resource")
+    public void createSmallfullDuplicateKey() throws DataException {
+        setUpCreate();
+        doReturn(currentPeriodEntity).when(currentPeriodTransformer).transform(ArgumentMatchers
+                .any(CurrentPeriod.class));
+        when(currentPeriodRepository.insert(currentPeriodEntity)).thenThrow(duplicateKeyException);
+        ResponseObject response = currentPeriodService.create(currentPeriod, transaction, "", "");
+        assertNotNull(response);
+        assertEquals(response.getStatus(), ResponseStatus.DUPLICATE_KEY_ERROR);
+        assertNull(response.getData());
+    }
+
+    @Test
+    @DisplayName("Tests the mongo exception when creating a current period")
+    void createSmallfullMongoExceptionFailure() throws DataException {
+        setUpCreate();
+        doReturn(currentPeriodEntity).when(currentPeriodTransformer).transform(ArgumentMatchers
+                .any(CurrentPeriod.class));
+        when(currentPeriodRepository.insert(currentPeriodEntity)).thenThrow(mongoException);
+        Executable executable = ()->{currentPeriodService.create(currentPeriod, transaction, "", "");};
+        assertThrows(DataException.class, executable);
+    }
+
+    @Test
+    @DisplayName("Tests the successful find of a currentPeriod resource")
+    public void findCurrentPeriod() throws DataException {
+        when(currentPeriodRepository.findById("")).thenReturn(Optional.ofNullable(currentPeriodEntity));
+        when(currentPeriodTransformer.transform(currentPeriodEntity)).thenReturn(currentPeriod);
+        ResponseObject<CurrentPeriod> result = currentPeriodService
+                .findById("", "");
+        assertNotNull(result);
+        assertEquals(currentPeriod, result.getData());
+    }
+
+    @Test
+    @DisplayName("Tests mongo exception thrown on find of a currentPeriod resource")
+    public void findCurrentPeriodMongoException() throws DataException {
+        when(currentPeriodRepository.findById("")).thenThrow(mongoException);
+        Executable executable = ()->{currentPeriodService.findById("", "");};
+        assertThrows(DataException.class, executable);
     }
 }
--- src/test/java/uk/gov/companieshouse/api/accounts/service/impl/SmallFullServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/service/impl/SmallFullServiceImplTest.java
@@ -2,26 +2,44 @@ package uk.gov.companieshouse.api.accounts.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.mongodb.DuplicateKeyException;
+import com.mongodb.MongoException;
 import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.accounts.LinkType;
 import uk.gov.companieshouse.api.accounts.exception.DataException;
 import uk.gov.companieshouse.api.accounts.model.entity.SmallFullEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.SmallFull;
 import uk.gov.companieshouse.api.accounts.repository.SmallFullRepository;
 import uk.gov.companieshouse.api.accounts.service.CompanyAccountService;
 import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseStatus;
+import uk.gov.companieshouse.api.accounts.transaction.ApiErrorResponseException;
 import uk.gov.companieshouse.api.accounts.transaction.Transaction;
+import uk.gov.companieshouse.api.accounts.transaction.TransactionStatus;
 import uk.gov.companieshouse.api.accounts.transformer.SmallFullTransformer;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,24 +67,71 @@ public class SmallFullServiceImplTest {
     @Mock
     private CompanyAccountService companyAccountService;
 
+    @Mock
+    private DuplicateKeyException duplicateKeyException;
+
+    @Mock
+    private MongoException mongoException;
+
     @InjectMocks
     private SmallFullServiceImpl smallFullService;
 
-    @BeforeEach
-    public void setUp() {
+    public void setUpCreate() {
         smallFullService.setMessageDigest(messageDigest);
-        when(smallFullTransformer.transform(smallFull)).thenReturn(smallFullEntity);
         byte[] b = {12, 10, 56, 120, 13, 15};
         when(messageDigest.digest(any())).thenReturn(b);
     }
 
     @Test
     @DisplayName("Tests the successful creation of a smallFull resource")
-    public void canCreateAccount() throws DataException {
+    public void createAccountSuccess() throws DataException {
+        setUpCreate();
         when(smallFullTransformer.transform(smallFull)).thenReturn(smallFullEntity);
         ResponseObject<SmallFull> result = smallFullService.create(smallFull, transaction, "", "");
         assertNotNull(result);
         assertEquals(smallFull, result.getData());
+    }
 
+    @Test
+    @DisplayName("Tests the duplicate key when creating a smallFull resource")
+    public void createSmallfullDuplicateKey() throws DataException {
+        setUpCreate();
+        doReturn(smallFullEntity).when(smallFullTransformer).transform(ArgumentMatchers
+                .any(SmallFull.class));
+        when(smallFullRepository.insert(smallFullEntity)).thenThrow(duplicateKeyException);
+        ResponseObject response = smallFullService.create(smallFull, transaction, "", "");
+        assertNotNull(response);
+        assertEquals(response.getStatus(), ResponseStatus.DUPLICATE_KEY_ERROR);
+        assertNull(response.getData());
+    }
+
+    @Test
+    @DisplayName("Tests the mongo exception when creating a small full")
+    void createSmallfullMongoExceptionFailure() throws DataException {
+        setUpCreate();
+        doReturn(smallFullEntity).when(smallFullTransformer).transform(ArgumentMatchers
+                .any(SmallFull.class));
+        when(smallFullRepository.insert(smallFullEntity)).thenThrow(mongoException);
+        Executable executable = ()->{smallFullService.create(smallFull, transaction, "", "");};
+        assertThrows(DataException.class, executable);
+    }
+
+    @Test
+    @DisplayName("Tests the successful find of a small full resource")
+    public void findSmallfull() throws DataException {
+        when(smallFullRepository.findById("")).thenReturn(Optional.ofNullable(smallFullEntity));
+        when(smallFullTransformer.transform(smallFullEntity)).thenReturn(smallFull);
+        ResponseObject<SmallFull> result = smallFullService
+                .findById("", "");
+        assertNotNull(result);
+        assertEquals(smallFull, result.getData());
+    }
+
+    @Test
+    @DisplayName("Tests mongo exception thrown on find of a small full resource")
+    public void findSmallfullMongoException() throws DataException {
+        when(smallFullRepository.findById("")).thenThrow(mongoException);
+        Executable executable = ()->{smallFullService.findById("", "");};
+        assertThrows(DataException.class, executable);
     }
 }
