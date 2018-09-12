99b163c41db34e855055679d72c725314775eaaa
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-12 15:37:38 +0100

    Add currentPeriodValidator to company accounts api
    Add current period validator class to validate current Period fields.
    Validate fixed Assets values on balance sheet

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/CurrentPeriodValidator.java
@@ -0,0 +1,12 @@
+package uk.gov.companieshouse.api.accounts.validation;
+
+
+/**
+ * Holds common balance sheet validator methods
+ */
+public abstract class BaseValidator implements CurrentPeriodValidator {
+
+
+
+
+}
