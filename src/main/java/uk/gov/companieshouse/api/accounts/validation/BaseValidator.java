049a1f7a0f01cf4c6675e3a840f613d02f388a01
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-14 10:35:19 +0100

    Remove methods to validate range of input
    Now using spring @Range annotations

--- src/main/java/uk/gov/companieshouse/api/accounts/validation/BaseValidator.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/BaseValidator.java
@@ -1,25 +1,9 @@
 package uk.gov.companieshouse.api.accounts.validation;
 
 import uk.gov.companieshouse.api.accounts.model.Errors;
-import uk.gov.companieshouse.api.accounts.model.NumericRange;
 
 
 public class BaseValidator {
-
-    /**
-     * Validate the optional value is within the given {@link NumericRange}
-     *
-     * @param value
-     * @param range
-     * @param errors
-     * @param location
-     */
-    protected void validateOptionalWithinRange(Integer value, NumericRange range, Errors errors,
-        String location) {
-        if (value != null && !range.inRangeInclusive(value)) {
-            addRangeError(errors, location, range);
-        }
-    }
 
     /**
      * Validate the given total is correctly aggregated
@@ -38,21 +22,6 @@ public class BaseValidator {
         } else if (total == null || !total.equals(expectedTotal)) {
             addIncorrectTotalError(errors, location);
         }
-    }
-
-    /**
-     * Add a range error for the given location
-     *
-     * @param errors
-     * @param location
-     * @param range
-     */
-    protected void addRangeError(Errors errors, String location, NumericRange range) {
-        Error error = new Error(ErrorMessageKeys.VALUE_OUTSIDE_RANGE.getKey(), location,
-            LocationType.JSON_PATH.getValue(), ErrorType.VALIDATION.getType());
-        error.addErrorValue("lower", Integer.toString(range.getStart()));
-        error.addErrorValue("upper", Integer.toString(range.getEnd()));
-        errors.addError(error);
     }
 
     /**
