bf89b681f678773488ac75d70b4ff099040ff8a5
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-14 10:19:33 +0100

    Handle validation in create controller Map binding errors to our Error model Call currentPeriodValidator class to validate totals

--- src/main/java/uk/gov/companieshouse/api/accounts/controller/CurrentPeriodController.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/controller/CurrentPeriodController.java
@@ -1,21 +1,17 @@
 package uk.gov.companieshouse.api.accounts.controller;
 
-import static uk.gov.companieshouse.api.accounts.CompanyAccountsApplication.APPLICATION_NAME_SPACE;
-
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import uk.gov.companieshouse.api.accounts.AttributeName;
 import uk.gov.companieshouse.api.accounts.exception.DataException;
 import uk.gov.companieshouse.api.accounts.model.Errors;
@@ -23,14 +19,21 @@ import uk.gov.companieshouse.api.accounts.model.entity.CompanyAccountEntity;
 import uk.gov.companieshouse.api.accounts.model.rest.CurrentPeriod;
 import uk.gov.companieshouse.api.accounts.service.CurrentPeriodService;
 import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
-import uk.gov.companieshouse.api.accounts.service.response.ValidationErrorData;
 import uk.gov.companieshouse.api.accounts.transaction.Transaction;
 import uk.gov.companieshouse.api.accounts.utility.ApiResponseMapper;
+import uk.gov.companieshouse.api.accounts.utility.BindingResultErrorToErrorMapper;
 import uk.gov.companieshouse.api.accounts.validation.CurrentPeriodValidator;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.logging.util.LogContext;
 import uk.gov.companieshouse.logging.util.LogHelper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+import static uk.gov.companieshouse.api.accounts.CompanyAccountsApplication.APPLICATION_NAME_SPACE;
 
 @RestController
 @RequestMapping(value = "/transactions/{transactionId}/company-accounts/{companyAccountId}/small-full/current-period", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -46,52 +49,50 @@ public class CurrentPeriodController {
     private CurrentPeriodValidator currentPeriodValidator;
 
     @Autowired
+    private BindingResultErrorToErrorMapper resultsMapper;
+
+    @Autowired
     private ApiResponseMapper apiResponseMapper;
-
+    
     @PostMapping
-    public ResponseEntity create(@Valid @RequestBody CurrentPeriod currentPeriod,
-            HttpServletRequest request, BindingResult bindingResult) {
+    public ResponseEntity create(@RequestBody @Valid CurrentPeriod currentPeriod,
+        BindingResult bindingResult, HttpServletRequest request) {
 
-        //Create errors object
+        Errors errors = new Errors();
 
-        // If binding result has errors
+        if (bindingResult.hasErrors()) {
 
-        // Map binding result to error object
+            errors = resultsMapper.mapBindingResultErrorsToErrorModel(bindingResult, errors);
 
-        //return errors object
-
-
-
-
-
-
-
-
-
-
-        Transaction transaction = (Transaction) request
-                .getAttribute(AttributeName.TRANSACTION.getValue());
-
-        CompanyAccountEntity companyAccountEntity = (CompanyAccountEntity) request
-                .getAttribute(AttributeName.COMPANY_ACCOUNT.getValue());
-
-        String companyAccountId = companyAccountEntity.getId();
-        String requestId = request.getHeader("X-Request-Id");
-
-        Errors errors = currentPeriodValidator.validateCurrentPeriod(currentPeriod);
-        if (errors.hasErrors()){
-            LOGGER.error("Current period validation failure");
-            logValidationFailureError(getRequestId(request),errors);
-            return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
         }
 
+        currentPeriodValidator.validateCurrentPeriod(currentPeriod, errors);
+        if (errors.hasErrors()) {
+
+            LOGGER.error("Current period validation failure");
+            logValidationFailureError(getRequestId(request), errors);
+
+            return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
+
+        }
+
+        Transaction transaction = (Transaction) request
+            .getAttribute(AttributeName.TRANSACTION.getValue());
+
+        CompanyAccountEntity companyAccountEntity = (CompanyAccountEntity) request
+            .getAttribute(AttributeName.COMPANY_ACCOUNT.getValue());
+
+        String companyAccountId = companyAccountEntity.getId();
+        String requestId = request.getHeader(REQUEST_ID);
+
         ResponseEntity responseEntity;
+
         try {
             ResponseObject<CurrentPeriod> responseObject = currentPeriodService
-                    .create(currentPeriod, transaction, companyAccountId, requestId);
+                .create(currentPeriod, transaction, companyAccountId, requestId);
             responseEntity = apiResponseMapper
-                    .map(responseObject.getStatus(), responseObject.getData(),
-                            responseObject.getValidationErrorData());
+                .map(responseObject.getStatus(), responseObject.getData(),
+                    responseObject.getValidationErrorData());
 
 
         } catch (DataException ex) {
@@ -109,13 +110,13 @@ public class CurrentPeriodController {
         LogContext logContext = LogHelper.createNewLogContext(request);
 
         Transaction transaction = (Transaction) request
-                .getAttribute(AttributeName.TRANSACTION.getValue());
+            .getAttribute(AttributeName.TRANSACTION.getValue());
         CompanyAccountEntity companyAccountEntity = (CompanyAccountEntity) request
-                .getAttribute(AttributeName.COMPANY_ACCOUNT.getValue());
+            .getAttribute(AttributeName.COMPANY_ACCOUNT.getValue());
 
         if (companyAccountEntity == null) {
             LOGGER.error("Current Period error: No company account in request session",
-                    logContext);
+                logContext);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
@@ -144,14 +145,15 @@ public class CurrentPeriodController {
      * @param requestId
      * @param errors
      */
+
     private void logValidationFailureError(String requestId, Errors errors) {
-        HashMap<String,Object> logMap = new HashMap<>();
+        HashMap<String, Object> logMap = new HashMap<>();
         logMap.put("message", "Validation failure");
         logMap.put("Errors: ", errors);
         LOGGER.traceContext(requestId, "", logMap);
     }
 
     private String getRequestId(HttpServletRequest request) {
-        return request.getHeader(REQUEST_ID) ;
+        return request.getHeader(REQUEST_ID);
     }
 }
