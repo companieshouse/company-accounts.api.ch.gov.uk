105480a210123aaa6dcd90bee8e46543255d3f70
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-14 10:22:26 +0100

    Add custom message to Balance sheets @NotNull annotaton
    Add message to map to MessageKey enums for Error model mapping

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/CurrentPeriod.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/CurrentPeriod.java
@@ -1,12 +1,15 @@
 package uk.gov.companieshouse.api.accounts.model.rest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 public class CurrentPeriod extends RestObject {
 
-    @NotNull
+    @NotNull(message="MANDATORY_ELEMENT_MISSING")
     @JsonProperty("balance_sheet")
+    @Valid
     private BalanceSheet balanceSheet;
 
     public BalanceSheet getBalanceSheet() {
