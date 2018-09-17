de4d011360d6eae4681c18940dcc7f7d58116e11
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-17 10:35:20 +0100

    use static junit Assertion imports for improved readibilty in tests Correct formatting of constants

--- src/test/java/uk/gov/companieshouse/api/accounts/transformer/CurrentPeriodTransformerTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/transformer/CurrentPeriodTransformerTest.java
@@ -2,7 +2,6 @@ package uk.gov.companieshouse.api.accounts.transformer;
 
 import java.util.HashMap;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -17,14 +16,18 @@ import uk.gov.companieshouse.api.accounts.model.rest.BalanceSheet;
 import uk.gov.companieshouse.api.accounts.model.rest.CurrentPeriod;
 import uk.gov.companieshouse.api.accounts.model.rest.FixedAssets;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(Lifecycle.PER_CLASS)
 public class CurrentPeriodTransformerTest {
 
-    private static final Integer CalledUpShareCapitalNotPaidValid = 5;
-    private static final Integer TangibleValid = 10;
-    private static final Integer FixedAssetsTotalValid = 10;
+    private static final Integer CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID = 5;
+    private static final Integer TANGIBLE_VALID = 10;
+    private static final Integer FIXED_ASSETS_TOTAL_VALID = 10;
 
     private CurrentPeriodTransformer currentPeriodTransformer = new CurrentPeriodTransformer();
 
@@ -34,21 +37,21 @@ public class CurrentPeriodTransformerTest {
         CurrentPeriodEntity companyAccountEntity = currentPeriodTransformer
             .transform(new CurrentPeriod());
 
-        Assertions.assertNotNull(companyAccountEntity);
-        Assertions.assertNull(companyAccountEntity.getData().getEtag());
-        Assertions.assertEquals(new HashMap<>(), companyAccountEntity.getData().getLinks());
+        assertNotNull(companyAccountEntity);
+        assertNull(companyAccountEntity.getData().getEtag());
+        assertEquals(new HashMap<>(), companyAccountEntity.getData().getLinks());
 
     }
 
     @Test
     @DisplayName("Tests current period transformer with populated object and validates values returned")
     public void testRestToEntityTransformerWithPopulatedObject() {
-        BalanceSheet balanceSheet = new BalanceSheet();
-        balanceSheet.setCalledUpShareCapitalNotPaid(CalledUpShareCapitalNotPaidValid);
 
+        BalanceSheet balanceSheet = new BalanceSheet();
+        balanceSheet.setCalledUpShareCapitalNotPaid(CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID);
         FixedAssets fixedAssets = new FixedAssets();
-        fixedAssets.setTangible(TangibleValid);
-        fixedAssets.setTotalFixedAssets(FixedAssetsTotalValid);
+        fixedAssets.setTangible(TANGIBLE_VALID);
+        fixedAssets.setTotalFixedAssets(FIXED_ASSETS_TOTAL_VALID);
 
         balanceSheet.setFixedAssets(fixedAssets);
 
@@ -58,33 +61,28 @@ public class CurrentPeriodTransformerTest {
         currentPeriod.setLinks(new HashMap<>());
         currentPeriod.setBalanceSheet(balanceSheet);
 
-        CurrentPeriodEntity currentPeriodEntity = currentPeriodTransformer
-            .transform(currentPeriod);
+        CurrentPeriodEntity currentPeriodEntity = currentPeriodTransformer.transform(currentPeriod);
 
         CurrentPeriodDataEntity data = currentPeriodEntity.getData();
 
-        Assertions.assertNotNull(currentPeriodEntity);
-        Assertions.assertEquals("etag", data.getEtag());
-        Assertions.assertEquals(Integer.valueOf(CalledUpShareCapitalNotPaidValid),
-            data.getBalanceSheetEntity()
-                .getCalledUpShareCapitalNotPaid());
-        Assertions.assertEquals(Integer.valueOf(TangibleValid),
-            data.getBalanceSheetEntity().getFixedAssets().getTangible());
-        Assertions.assertEquals(Integer.valueOf(FixedAssetsTotalValid),
-            data.getBalanceSheetEntity().getFixedAssets().getTotalFixedAssets());
-        Assertions.assertEquals("kind", data.getKind());
-        Assertions.assertEquals(new HashMap<>(), data.getLinks());
+        assertNotNull(currentPeriodEntity);
+        assertEquals("etag", data.getEtag());
+        assertEquals(CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID, data.getBalanceSheetEntity().getCalledUpShareCapitalNotPaid());
+        assertEquals(TANGIBLE_VALID, data.getBalanceSheetEntity().getFixedAssets().getTangible());
+        assertEquals(FIXED_ASSETS_TOTAL_VALID, data.getBalanceSheetEntity().getFixedAssets().getTotalFixedAssets());
+        assertEquals("kind", data.getKind());
+        assertEquals(new HashMap<>(), data.getLinks());
     }
 
     @Test
     @DisplayName("Tests current period transformer with populated object and validates values returned")
     public void testEntityToRestTransformerWithPopulatedObject() {
         BalanceSheetEntity balanceSheetEntity = new BalanceSheetEntity();
-        balanceSheetEntity.setCalledUpShareCapitalNotPaid(CalledUpShareCapitalNotPaidValid);
+        balanceSheetEntity.setCalledUpShareCapitalNotPaid(CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID);
 
         FixedAssetsEntity fixedAssetsEntity = new FixedAssetsEntity();
-        fixedAssetsEntity.setTangible(TangibleValid);
-        fixedAssetsEntity.setTotalFixedAssets(FixedAssetsTotalValid);
+        fixedAssetsEntity.setTangible(TANGIBLE_VALID);
+        fixedAssetsEntity.setTotalFixedAssets(FIXED_ASSETS_TOTAL_VALID);
 
         balanceSheetEntity.setFixedAssets(fixedAssetsEntity);
 
@@ -96,20 +94,15 @@ public class CurrentPeriodTransformerTest {
         currentPeriodDataEntity.setBalanceSheetEntity(balanceSheetEntity);
         currentPeriodEntity.setData(currentPeriodDataEntity);
 
-        CurrentPeriod currentPeriod = currentPeriodTransformer
-            .transform(currentPeriodEntity);
+        CurrentPeriod currentPeriod = currentPeriodTransformer.transform(currentPeriodEntity);
 
-        Assertions.assertNotNull(currentPeriodEntity);
-        Assertions.assertEquals("etag", currentPeriod.getEtag());
-        Assertions.assertEquals(Integer.valueOf(CalledUpShareCapitalNotPaidValid),
-            currentPeriod.getBalanceSheet()
-                .getCalledUpShareCapitalNotPaid());
-        Assertions.assertEquals(Integer.valueOf(TangibleValid),
-            currentPeriod.getBalanceSheet().getFixedAssets().getTangible());
-        Assertions.assertEquals(Integer.valueOf(FixedAssetsTotalValid),
-            currentPeriod.getBalanceSheet().getFixedAssets().getTotalFixedAssets());
-        Assertions.assertEquals("kind", currentPeriod.getKind());
-        Assertions.assertEquals(new HashMap<>(), currentPeriod.getLinks());
+        assertNotNull(currentPeriodEntity);
+        assertEquals("etag", currentPeriod.getEtag());
+        assertEquals(CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID, currentPeriod.getBalanceSheet().getCalledUpShareCapitalNotPaid());
+        assertEquals(TANGIBLE_VALID, currentPeriod.getBalanceSheet().getFixedAssets().getTangible());
+        assertEquals(FIXED_ASSETS_TOTAL_VALID, currentPeriod.getBalanceSheet().getFixedAssets().getTotalFixedAssets());
+        assertEquals("kind", currentPeriod.getKind());
+        assertEquals(new HashMap<>(), currentPeriod.getLinks());
     }
 }
 
