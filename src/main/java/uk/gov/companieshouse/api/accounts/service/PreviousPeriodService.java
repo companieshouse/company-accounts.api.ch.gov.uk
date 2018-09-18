6acc467c31d179ea61ff72065f4ea4bfaccc24e8
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-18 13:32:26 +0100

    Add previousPeriod service to create previous periods in Mongo

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/service/PreviousPeriodService.java
@@ -0,0 +1,10 @@
+package uk.gov.companieshouse.api.accounts.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.api.accounts.model.rest.PreviousPeriod;
+import uk.gov.companieshouse.api.accounts.service.AbstractService;
+
+@Service
+public interface PreviousPeriodService extends AbstractService<PreviousPeriod> {
+
+}
---
+++ src/main/java/uk/gov/companieshouse/api/accounts/service/impl/PreviousPeriodServiceImpl.java
@@ -0,0 +1,140 @@
+package uk.gov.companieshouse.api.accounts.service.impl;
+
+import com.mongodb.DuplicateKeyException;
+import com.mongodb.MongoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.GenerateEtagUtil;
+import uk.gov.companieshouse.api.accounts.CompanyAccountsApplication;
+import uk.gov.companieshouse.api.accounts.Kind;
+import uk.gov.companieshouse.api.accounts.LinkType;
+import uk.gov.companieshouse.api.accounts.ResourceName;
+import uk.gov.companieshouse.api.accounts.exception.DataException;
+import uk.gov.companieshouse.api.accounts.model.entity.CurrentPeriodEntity;
+import uk.gov.companieshouse.api.accounts.model.entity.PreviousPeriodEntity;
+import uk.gov.companieshouse.api.accounts.model.rest.PreviousPeriod;
+import uk.gov.companieshouse.api.accounts.model.rest.PreviousPeriod;
+import uk.gov.companieshouse.api.accounts.repository.PreviousPeriodRespository;
+import uk.gov.companieshouse.api.accounts.service.PreviousPeriodService;
+import uk.gov.companieshouse.api.accounts.service.SmallFullService;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseStatus;
+import uk.gov.companieshouse.api.accounts.transaction.Transaction;
+import uk.gov.companieshouse.api.accounts.transformer.PreviousPeriodTransformer;
+import uk.gov.companieshouse.api.accounts.utility.impl.KeyIdGenerator;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class PreviousPeriodServiceImpl implements PreviousPeriodService {
+
+    private static final Logger LOGGER = LoggerFactory
+        .getLogger(CompanyAccountsApplication.APPLICATION_NAME_SPACE);
+
+    private PreviousPeriodRespository previousPeriodRespository;
+
+    private PreviousPeriodTransformer previousPeriodTransformer;
+
+    private SmallFullService smallFullService;
+
+    private KeyIdGenerator keyIdGenerator;
+
+    @Autowired
+    public PreviousPeriodServiceImpl (
+        PreviousPeriodRespository previousPeriodRepository,
+        PreviousPeriodTransformer previousPeriodTransformer,
+        SmallFullService smallFullService,
+        KeyIdGenerator keyIdGenerator){
+       this.previousPeriodRespository = previousPeriodRepository;
+        this.previousPeriodTransformer = previousPeriodTransformer;
+        this.smallFullService = smallFullService;
+        this.keyIdGenerator = keyIdGenerator;
+    }
+
+    @Override
+    public ResponseObject<PreviousPeriod> create(PreviousPeriod previousPeriod, Transaction transaction,
+        String companyAccountId, String requestId) throws DataException {
+        
+        String selfLink = createSelfLink(transaction, companyAccountId);
+        initLinks(previousPeriod, selfLink);
+        previousPeriod.setEtag(GenerateEtagUtil.generateEtag());
+        previousPeriod.setKind(Kind.PREVIOUS_PERIOD.getValue());
+        PreviousPeriodEntity previousPeriodEntity = previousPeriodTransformer.transform(previousPeriod);
+
+        final Map<String, Object> debugMap = new HashMap<>();
+        debugMap.put("transaction_id", transaction.getId());
+        debugMap.put("company_accounts_id", companyAccountId);
+
+        String id = generateID(companyAccountId);
+        previousPeriodEntity.setId(id);
+        debugMap.put("id", id);
+
+        try {
+            previousPeriodRespository.insert(previousPeriodEntity);
+        } catch (DuplicateKeyException dke) {
+            LOGGER.errorContext(requestId, dke, debugMap);
+            return new ResponseObject<>(ResponseStatus.DUPLICATE_KEY_ERROR, null);
+        } catch (MongoException me) {
+            DataException dataException = new DataException(
+                "Failed to insert " + ResourceName.SMALL_FULL.getName(), me);
+            LOGGER.errorContext(requestId, dataException, debugMap);
+            throw dataException;
+        }
+
+        smallFullService.addLink(companyAccountId, LinkType.SMALL_FULL, selfLink, requestId);
+
+        return new ResponseObject<>(ResponseStatus.CREATED, previousPeriod);
+    }
+
+    @Override
+    public ResponseObject<PreviousPeriod> findById(String id, String requestId){
+
+        return null;
+    }
+
+    @Override
+    public void addLink(String id, LinkType linkType, String link, String requestId)
+        throws DataException {
+        String previousPeriodId = generateID(id);
+        PreviousPeriodEntity previousPeriodEntity = previousPeriodRespository.findById(previousPeriodId)
+            .orElseThrow(() -> new DataException(
+                "Failed to add get Previous period entity to add link"));
+        previousPeriodEntity.getData().getLinks().put(linkType.getLink(), link);
+
+        try {
+            previousPeriodRespository.save(previousPeriodEntity);
+        } catch (MongoException me) {
+            final Map<String, Object> debugMap = new HashMap<>();
+            debugMap.put("company_account_id", id);
+            debugMap.put("id", previousPeriodId);
+            debugMap.put("link", link);
+            debugMap.put("link_type", linkType.getLink());
+
+            DataException dataException = new DataException(
+                "Failed to add link to Small full", me);
+            LOGGER.errorContext(requestId, dataException, debugMap);
+            throw dataException;
+        }
+    }
+
+    @Override
+    public String generateID(String value) {
+        return keyIdGenerator.generate(value + "-" + ResourceName.PREVIOUS_PERIOD.getName());
+    }
+
+    public String createSelfLink(Transaction transaction, String companyAccountId) {
+        return transaction.getLinks().get(LinkType.SELF.getLink()) + "/"
+            + ResourceName.COMPANY_ACCOUNT.getName() + "/"
+            + companyAccountId + "/" + ResourceName.SMALL_FULL.getName() + "/"
+            + ResourceName.PREVIOUS_PERIOD.getName();
+    }
+
+    private void initLinks(PreviousPeriod previousPeriod, String link) {
+        Map<String, String> map = new HashMap<>();
+        map.put(LinkType.SELF.getLink(), link);
+        previousPeriod.setLinks(map);
+    }
+}
