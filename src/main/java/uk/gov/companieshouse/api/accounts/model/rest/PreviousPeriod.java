5b6b9eaae8a8cbcea590affae9b943c8369c4afd
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-18 14:42:59 +0100

    Add @JsonInclude(Include.NON_NULL) to previous period rest model, to avoid null objects being mapped

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/PreviousPeriod.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/PreviousPeriod.java
@@ -1,9 +1,12 @@
 package uk.gov.companieshouse.api.accounts.model.rest;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PreviousPeriod extends RestObject {
 
+    @JsonInclude(Include.NON_NULL)
     @JsonProperty("balance_sheet")
     private BalanceSheet balanceSheet;
 
