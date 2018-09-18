9937dc0f8eddbe7901979d033fb0ea3cd989f998
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-18 14:45:17 +0100

    Readd @notNull annotation to rest balancesheet model

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/BalanceSheet.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/BalanceSheet.java
@@ -1,10 +1,12 @@
 package uk.gov.companieshouse.api.accounts.model.rest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.validation.constraints.NotNull;
 
 public class BalanceSheet {
-
+    
+    @NotNull
     @JsonProperty("called_up_share_capital_not_paid")
     private Integer calledUpShareCapitalNotPaid;
 
