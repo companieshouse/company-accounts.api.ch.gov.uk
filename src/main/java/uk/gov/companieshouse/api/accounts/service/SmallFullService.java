e11cccd73b556dc6f7a2051c012fb6b1b32c1428
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-09-18 10:40:06 +0100

    removed one level of interface

--- src/main/java/uk/gov/companieshouse/api/accounts/service/SmallFullService.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.companieshouse.api.accounts.service;
-
-import org.springframework.stereotype.Service;
-import uk.gov.companieshouse.api.accounts.model.rest.SmallFull;
-
-@Service
-public interface SmallFullService extends AbstractParentService<SmallFull> {
-
-}
