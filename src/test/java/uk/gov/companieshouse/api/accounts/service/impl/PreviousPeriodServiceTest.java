3a93d0920c72c58248f3eda547994507d1782e59
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-20 09:15:35 +0100

    Remove empty setup method from tests

--- src/test/java/uk/gov/companieshouse/api/accounts/service/impl/PreviousPeriodServiceTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/service/impl/PreviousPeriodServiceTest.java
@@ -68,13 +68,10 @@ public class PreviousPeriodServiceTest {
     @InjectMocks
     private PreviousPeriodService previousPeriodService;
 
-    public void setUpCreate() {
-    }
 
     @Test
     @DisplayName("Tests the successful creation of a previousPeriod resource")
     public void canCreatePreviousPeriod() throws DataException {
-        setUpCreate();
         when(previousPeriodTransformer.transform(previousPeriod)).thenReturn(previousPeriodEntity);
         ResponseObject<PreviousPeriod> result = previousPeriodService
             .create(previousPeriod, transaction, "", "");
@@ -85,7 +82,6 @@ public class PreviousPeriodServiceTest {
     @Test
     @DisplayName("Tests the duplicate key when creating a previous period resource")
     public void createSmallfullDuplicateKey() throws DataException {
-        setUpCreate();
         doReturn(previousPeriodEntity).when(previousPeriodTransformer).transform(ArgumentMatchers
             .any(PreviousPeriod.class));
         when(previousPeriodRepository.insert(previousPeriodEntity)).thenThrow(duplicateKeyException);
@@ -98,7 +94,6 @@ public class PreviousPeriodServiceTest {
     @Test
     @DisplayName("Tests the mongo exception when creating a previous period")
     void createSmallfullMongoExceptionFailure() throws DataException {
-        setUpCreate();
         doReturn(previousPeriodEntity).when(previousPeriodTransformer).transform(ArgumentMatchers
             .any(PreviousPeriod.class));
         when(previousPeriodRepository.insert(previousPeriodEntity)).thenThrow(mongoException);
@@ -107,6 +102,4 @@ public class PreviousPeriodServiceTest {
         };
         assertThrows(DataException.class, executable);
     }
-
-
 }
