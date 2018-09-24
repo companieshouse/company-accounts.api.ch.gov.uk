4afaf3f576b97b6449514c11109f89acdf3960e0
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-09-24 12:09:36 +0100

    added approvals models

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/entity/ApprovalEntity.java
@@ -0,0 +1,19 @@
+package uk.gov.companieshouse.api.accounts.model.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "approvals")
+public class ApprovalEntity extends BaseEntity {
+
+    @Field("data")
+    private ApprovalDataEntity data;
+
+    public ApprovalDataEntity getData() {
+        return data;
+    }
+
+    public void setData(ApprovalDataEntity data) {
+        this.data = data;
+    }
+}
