ba131c9fec4716166ffde4fe3f68594f895bb430
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-09-27 14:03:27 +0100

    Adding service layer class and unit tests

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/service/impl/ApprovalService.java
@@ -0,0 +1,125 @@
+package uk.gov.companieshouse.api.accounts.service.impl;
+
+import com.mongodb.MongoException;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import uk.gov.companieshouse.GenerateEtagUtil;
+import uk.gov.companieshouse.api.accounts.CompanyAccountsApplication;
+import uk.gov.companieshouse.api.accounts.Kind;
+import uk.gov.companieshouse.api.accounts.ResourceName;
+import uk.gov.companieshouse.api.accounts.exception.DataException;
+import uk.gov.companieshouse.api.accounts.links.BasicLinkType;
+import uk.gov.companieshouse.api.accounts.links.SmallFullLinkType;
+import uk.gov.companieshouse.api.accounts.links.TransactionLinkType;
+import uk.gov.companieshouse.api.accounts.model.entity.ApprovalEntity;
+import uk.gov.companieshouse.api.accounts.model.rest.Approval;
+import uk.gov.companieshouse.api.accounts.repository.ApprovalRepository;
+import uk.gov.companieshouse.api.accounts.service.ResourceService;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseStatus;
+import uk.gov.companieshouse.api.accounts.transaction.Transaction;
+import uk.gov.companieshouse.api.accounts.transformer.ApprovalTransformer;
+import uk.gov.companieshouse.api.accounts.utility.impl.KeyIdGenerator;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+public class ApprovalService implements ResourceService<Approval> {
+
+    private static final Logger LOGGER = LoggerFactory
+        .getLogger(CompanyAccountsApplication.APPLICATION_NAME_SPACE);
+
+    private ApprovalRepository approvalRepository;
+
+    private ApprovalTransformer approvalTransformer;
+
+    private SmallFullService smallFullService;
+
+    private KeyIdGenerator keyIdGenerator;
+
+    @Autowired
+    public ApprovalService(
+        ApprovalRepository approvalRepository,
+        ApprovalTransformer approvalTransformer,
+        SmallFullService smallFullService,
+        KeyIdGenerator keyIdGenerator) {
+        this.approvalRepository = approvalRepository;
+        this.approvalTransformer = approvalTransformer;
+        this.smallFullService = smallFullService;
+        this.keyIdGenerator = keyIdGenerator;
+    }
+
+    @Override
+    public ResponseObject<Approval> create(Approval rest, Transaction transaction,
+        String companyAccountId, String requestId) throws DataException {
+        String selfLink = createSelfLink(transaction, companyAccountId);
+        initLinks(rest, selfLink);
+        rest.setEtag(GenerateEtagUtil.generateEtag());
+        rest.setKind(Kind.CURRENT_PERIOD.getValue());
+        ApprovalEntity approvalEntity = approvalTransformer.transform(rest);
+
+        final Map<String, Object> debugMap = new HashMap<>();
+        debugMap.put("transaction_id", transaction.getId());
+        debugMap.put("company_accounts_id", companyAccountId);
+
+        String id = generateID(companyAccountId);
+        approvalEntity.setId(id);
+        debugMap.put("id", id);
+
+        try {
+            approvalRepository.insert(approvalEntity);
+        } catch (DuplicateKeyException dke) {
+            LOGGER.errorContext(requestId, dke, debugMap);
+            return new ResponseObject<>(ResponseStatus.DUPLICATE_KEY_ERROR, null);
+        } catch (MongoException me) {
+            DataException dataException = new DataException(
+                "Failed to insert " + ResourceName.SMALL_FULL.getName(), me);
+            LOGGER.errorContext(requestId, dataException, debugMap);
+            throw dataException;
+        }
+
+        smallFullService
+            .addLink(companyAccountId, SmallFullLinkType.CURRENT_PERIOD, selfLink, requestId);
+
+        return new ResponseObject<>(ResponseStatus.CREATED, rest);
+    }
+
+    @Override
+    public ResponseObject<Approval> findById(String id, String requestId) throws DataException {
+        ApprovalEntity approvalEntity;
+        try {
+            approvalEntity = approvalRepository.findById(id).orElse(null);
+        } catch (MongoException me) {
+            final Map<String, Object> debugMap = new HashMap<>();
+            debugMap.put("id", id);
+            DataException dataException = new DataException("Failed to find Approval", me);
+            LOGGER.errorContext(requestId, dataException, debugMap);
+            throw dataException;
+        }
+
+        if (approvalEntity == null) {
+            return new ResponseObject<>(ResponseStatus.NOT_FOUND);
+        }
+        Approval approval = approvalTransformer.transform(approvalEntity);
+        return new ResponseObject<>(ResponseStatus.FOUND, approval);
+    }
+
+    @Override
+    public String generateID(String companyAccountId) {
+        return keyIdGenerator.generate(companyAccountId + "-" + ResourceName.APPROVAL.getName());
+    }
+
+    public String createSelfLink(Transaction transaction, String companyAccountId) {
+        return transaction.getLinks().get(TransactionLinkType.SELF.getLink()) + "/"
+            + ResourceName.COMPANY_ACCOUNT.getName() + "/"
+            + companyAccountId + "/" + ResourceName.SMALL_FULL.getName() + "/"
+            + ResourceName.APPROVAL.getName();
+    }
+
+    private void initLinks(Approval approval, String link) {
+        Map<String, String> map = new HashMap<>();
+        map.put(BasicLinkType.SELF.getLink(), link);
+        approval.setLinks(map);
+    }
+}
