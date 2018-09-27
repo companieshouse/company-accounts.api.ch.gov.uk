b37994c30a2075520a18686a7eba882ede316c28
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-27 09:51:42 +0100

    Add fixed assets and tangible to ixbrl model

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/ixbrl/balancesheet/FixedAssets.java
@@ -0,0 +1,37 @@
+package uk.gov.companieshouse.api.accounts.model.ixbrl.balancesheet;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class FixedAssets {
+
+    @JsonProperty("tangible")
+    public TangibleAssets tangibleAssets;
+
+    @JsonProperty("current_total")
+    public int totalFixedAssetsCurrent;
+
+    @JsonProperty("previous_total")
+    public int totalFixedAssetsPrevious;
+
+    public TangibleAssets getTangibleAssets() { return tangibleAssets; }
+
+    public void setTangibleAssets( TangibleAssets tangibleAssets) {
+        this.tangibleAssets = tangibleAssets;
+    }
+
+    public int getTotalFixedAssetsCurrent() {
+        return totalFixedAssetsCurrent;
+    }
+
+    public void setTotalFixedAssetsCurrent(int totalFixedAssetsCurrent) {
+        this.totalFixedAssetsCurrent = totalFixedAssetsCurrent;
+    }
+
+    public int getTotalFixedAssetsPrevious() {
+        return totalFixedAssetsPrevious;
+    }
+
+    public void setTotalFixedAssetsPrevious(int totalFixedAssetsPrevious) {
+        this.totalFixedAssetsPrevious = totalFixedAssetsPrevious;
+    }
+}
---
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/ixbrl/balancesheet/TangibleAssets.java
@@ -0,0 +1,28 @@
+package uk.gov.companieshouse.api.accounts.model.ixbrl.balancesheet;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TangibleAssets {
+
+    @JsonProperty("current_amount")
+    private int currentAmount;
+    @JsonProperty("previous_amount")
+    private int previousAmount;
+
+    public int getCurrentAmount() {
+        return currentAmount;
+    }
+
+    public void setCurrentAmount(int currentAmount) {
+        this.currentAmount = currentAmount;
+    }
+
+    public int getPreviousAmount() {
+        return previousAmount;
+    }
+
+    public void setPreviousAmount(int previousAmount) {
+        this.previousAmount = previousAmount;
+    }
+}
+
