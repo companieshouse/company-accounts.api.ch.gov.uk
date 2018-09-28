ca7bf4160c543597b7ac82d5a8dfb12b0178eada
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-09-28 10:05:55 +0100

    Add previous period validator for validating fixed-assets totals

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/PreviousPeriodValidator.java
@@ -0,0 +1,48 @@
+package uk.gov.companieshouse.api.accounts.validation;
+
+import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.accounts.model.rest.CurrentPeriod;
+import uk.gov.companieshouse.api.accounts.model.rest.FixedAssets;
+import uk.gov.companieshouse.api.accounts.model.rest.PreviousPeriod;
+import uk.gov.companieshouse.api.accounts.model.validation.*;
+import uk.gov.companieshouse.api.accounts.model.validation.Error;
+
+
+import javax.validation.Valid;
+
+@Component
+public class PreviousPeriodValidator extends BaseValidator {
+
+    String PREVIOUS_PERIOD_PATH = "$.previous_period";
+    String BALANCE_SHEET_PATH = PREVIOUS_PERIOD_PATH + ".balance_sheet";
+    String FIXED_ASSETS_PATH = BALANCE_SHEET_PATH + ".fixed_assets";
+    String TOTAL_PATH = FIXED_ASSETS_PATH + ".total";
+
+    public uk.gov.companieshouse.api.accounts.model.validation.Errors validatePreviousPeriod(@Valid PreviousPeriod previousPeriod, uk.gov.companieshouse.api.accounts.model.validation.Errors errors) {
+
+        if (previousPeriod.getBalanceSheet() != null) {
+
+            validateTotalFixedAssets(previousPeriod, errors);
+        }
+
+        return errors;
+    }
+
+    private void validateTotalFixedAssets(@Valid PreviousPeriod previousPeriod,
+        uk.gov.companieshouse.api.accounts.model.validation.Errors errors) {
+        FixedAssets fixedAssets = previousPeriod.getBalanceSheet().getFixedAssets();
+        if (fixedAssets != null) {
+
+            Integer tangible = fixedAssets.getTangible();
+            Integer fixedAssetsTotal = fixedAssets.getTotalFixedAssets();
+
+            // Will calculate the total of all fixedAssets fields as they are added to the balance sheet
+            Integer calculatedTotal = tangible;
+
+            validateAggregateTotal(fixedAssetsTotal, calculatedTotal, TOTAL_PATH, errors);
+        }
+    }
+
+
+
+}
