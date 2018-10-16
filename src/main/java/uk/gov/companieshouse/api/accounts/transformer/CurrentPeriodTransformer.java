eef2cfd8bb4bc4c84a86715e02757492efc7192b
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-10-16 14:29:04 +0100

    Add current assets fields to transformer

--- src/main/java/uk/gov/companieshouse/api/accounts/transformer/CurrentPeriodTransformer.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/transformer/CurrentPeriodTransformer.java
@@ -2,11 +2,9 @@ package uk.gov.companieshouse.api.accounts.transformer;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
-import uk.gov.companieshouse.api.accounts.model.entity.BalanceSheetEntity;
-import uk.gov.companieshouse.api.accounts.model.entity.CurrentPeriodDataEntity;
-import uk.gov.companieshouse.api.accounts.model.entity.CurrentPeriodEntity;
-import uk.gov.companieshouse.api.accounts.model.entity.FixedAssetsEntity;
+import uk.gov.companieshouse.api.accounts.model.entity.*;
 import uk.gov.companieshouse.api.accounts.model.rest.BalanceSheet;
+import uk.gov.companieshouse.api.accounts.model.rest.CurrentAssets;
 import uk.gov.companieshouse.api.accounts.model.rest.CurrentPeriod;
 import uk.gov.companieshouse.api.accounts.model.rest.FixedAssets;
 
@@ -29,6 +27,13 @@ public class CurrentPeriodTransformer implements
                 BeanUtils
                     .copyProperties(entity.getBalanceSheet().getFixedAssets(), fixedAssetsEntity);
                 balanceSheetEntity.setFixedAssets(fixedAssetsEntity);
+            }
+
+            if (entity.getBalanceSheet().getCurrentAssets() != null) {
+                CurrentAssetsEntity currentAssetsEntity = new CurrentAssetsEntity();
+                BeanUtils
+                        .copyProperties(entity.getBalanceSheet().getCurrentAssets(), currentAssetsEntity);
+                balanceSheetEntity.setCurrentAssets(currentAssetsEntity);
             }
         }
 
@@ -53,6 +58,13 @@ public class CurrentPeriodTransformer implements
                 BeanUtils.copyProperties(
                     currentPeriodDataEntity.getBalanceSheetEntity().getFixedAssets(), fixedAssets);
                 balanceSheet.setFixedAssets(fixedAssets);
+            }
+
+            if (currentPeriodDataEntity.getBalanceSheetEntity().getCurrentAssets() != null) {
+                CurrentAssets currentAssets  = new CurrentAssets();
+                BeanUtils.copyProperties(
+                        currentPeriodDataEntity.getBalanceSheetEntity().getCurrentAssets(), currentAssets);
+                balanceSheet.setCurrentAssets(currentAssets);
             }
         }
 
--- src/test/java/uk/gov/companieshouse/api/accounts/transformer/CurrentPeriodTransformerTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/transformer/CurrentPeriodTransformerTest.java
@@ -8,11 +8,9 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.companieshouse.api.accounts.model.entity.BalanceSheetEntity;
-import uk.gov.companieshouse.api.accounts.model.entity.CurrentPeriodDataEntity;
-import uk.gov.companieshouse.api.accounts.model.entity.CurrentPeriodEntity;
-import uk.gov.companieshouse.api.accounts.model.entity.FixedAssetsEntity;
+import uk.gov.companieshouse.api.accounts.model.entity.*;
 import uk.gov.companieshouse.api.accounts.model.rest.BalanceSheet;
+import uk.gov.companieshouse.api.accounts.model.rest.CurrentAssets;
 import uk.gov.companieshouse.api.accounts.model.rest.CurrentPeriod;
 import uk.gov.companieshouse.api.accounts.model.rest.FixedAssets;
 
@@ -28,6 +26,8 @@ public class CurrentPeriodTransformerTest {
     private static final Long CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID = 5L;
     private static final Long TANGIBLE_VALID = 10L;
     private static final Long FIXED_ASSETS_TOTAL_VALID = 10L;
+    private static final Long VALID_VALUE = 100L;
+    private static final Long CURRENT_ASSETS_TOTAL = 300l;
 
     private CurrentPeriodTransformer currentPeriodTransformer = new CurrentPeriodTransformer();
 
@@ -49,11 +49,20 @@ public class CurrentPeriodTransformerTest {
 
         BalanceSheet balanceSheet = new BalanceSheet();
         balanceSheet.setCalledUpShareCapitalNotPaid(CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID);
+
         FixedAssets fixedAssets = new FixedAssets();
         fixedAssets.setTangible(TANGIBLE_VALID);
         fixedAssets.setTotalFixedAssets(FIXED_ASSETS_TOTAL_VALID);
 
         balanceSheet.setFixedAssets(fixedAssets);
+
+        CurrentAssets currentAssets = new CurrentAssets();
+        currentAssets.setStocks(VALID_VALUE);
+        currentAssets.setDebtors(VALID_VALUE);
+        currentAssets.setCashAtBankAndInHand(VALID_VALUE);
+        currentAssets.setTotalCurrentAssets(CURRENT_ASSETS_TOTAL);
+
+        balanceSheet.setCurrentAssets(currentAssets);
 
         CurrentPeriod currentPeriod = new CurrentPeriod();
         currentPeriod.setEtag("etag");
@@ -68,8 +77,15 @@ public class CurrentPeriodTransformerTest {
         assertNotNull(currentPeriodEntity);
         assertEquals("etag", data.getEtag());
         assertEquals(CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID, data.getBalanceSheetEntity().getCalledUpShareCapitalNotPaid());
+
         assertEquals(TANGIBLE_VALID, data.getBalanceSheetEntity().getFixedAssets().getTangible());
         assertEquals(FIXED_ASSETS_TOTAL_VALID, data.getBalanceSheetEntity().getFixedAssets().getTotalFixedAssets());
+
+        assertEquals(VALID_VALUE, data.getBalanceSheetEntity().getCurrentAssets().getStocks());
+        assertEquals(VALID_VALUE, data.getBalanceSheetEntity().getCurrentAssets().getDebtors());
+        assertEquals(VALID_VALUE, data.getBalanceSheetEntity().getCurrentAssets().getCashAtBankAndInHand());
+        assertEquals(CURRENT_ASSETS_TOTAL, data.getBalanceSheetEntity().getCurrentAssets().getTotalCurrentAssets());
+
         assertEquals("kind", data.getKind());
         assertEquals(new HashMap<>(), data.getLinks());
     }
@@ -86,6 +102,14 @@ public class CurrentPeriodTransformerTest {
 
         balanceSheetEntity.setFixedAssets(fixedAssetsEntity);
 
+        CurrentAssetsEntity currentAssetsEntity = new CurrentAssetsEntity();
+        currentAssetsEntity.setStocks(VALID_VALUE);
+        currentAssetsEntity.setDebtors(VALID_VALUE);
+        currentAssetsEntity.setCashAtBankAndInHand(VALID_VALUE);
+        currentAssetsEntity.setTotalCurrentAssets(CURRENT_ASSETS_TOTAL);
+
+        balanceSheetEntity.setCurrentAssets(currentAssetsEntity);
+
         CurrentPeriodEntity currentPeriodEntity = new CurrentPeriodEntity();
         CurrentPeriodDataEntity currentPeriodDataEntity = new CurrentPeriodDataEntity();
         currentPeriodDataEntity.setEtag("etag");
@@ -101,6 +125,10 @@ public class CurrentPeriodTransformerTest {
         assertEquals(CALLED_UP_SHARE_CAPITAL_NOT_PAID_VALID, currentPeriod.getBalanceSheet().getCalledUpShareCapitalNotPaid());
         assertEquals(TANGIBLE_VALID, currentPeriod.getBalanceSheet().getFixedAssets().getTangible());
         assertEquals(FIXED_ASSETS_TOTAL_VALID, currentPeriod.getBalanceSheet().getFixedAssets().getTotalFixedAssets());
+        assertEquals(VALID_VALUE, currentPeriod.getBalanceSheet().getCurrentAssets().getStocks());
+        assertEquals(VALID_VALUE, currentPeriod.getBalanceSheet().getCurrentAssets().getDebtors());
+        assertEquals(VALID_VALUE, currentPeriod.getBalanceSheet().getCurrentAssets().getCashAtBankAndInHand());
+        assertEquals(CURRENT_ASSETS_TOTAL, currentPeriod.getBalanceSheet().getCurrentAssets().getTotalCurrentAssets());
         assertEquals("kind", currentPeriod.getKind());
         assertEquals(new HashMap<>(), currentPeriod.getLinks());
     }
