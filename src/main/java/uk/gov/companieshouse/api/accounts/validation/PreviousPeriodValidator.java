7a83053f02ef0ca08671c69f3cab6c0a08e0a973
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-10-17 15:32:42 +0100

    Add previous current assets and validation to api

--- src/main/java/uk/gov/companieshouse/api/accounts/validation/PreviousPeriodValidator.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/PreviousPeriodValidator.java
@@ -1,6 +1,7 @@
 package uk.gov.companieshouse.api.accounts.validation;
 
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.accounts.model.rest.CurrentAssets;
 import uk.gov.companieshouse.api.accounts.model.rest.FixedAssets;
 import uk.gov.companieshouse.api.accounts.model.rest.PreviousPeriod;
 import uk.gov.companieshouse.api.accounts.model.validation.Errors;
@@ -10,10 +11,11 @@ import javax.validation.Valid;
 @Component
 public class PreviousPeriodValidator extends BaseValidator {
 
-    private static String  PREVIOUS_PERIOD_PATH = "$.previous_period";
+    private static String PREVIOUS_PERIOD_PATH = "$.previous_period";
     private static String BALANCE_SHEET_PATH = PREVIOUS_PERIOD_PATH + ".balance_sheet";
     private static String FIXED_ASSETS_PATH = BALANCE_SHEET_PATH + ".fixed_assets";
-    private static String TOTAL_PATH = FIXED_ASSETS_PATH + ".total";
+    private static String FIXED_ASSETS_TOTAL_PATH = FIXED_ASSETS_PATH + ".total";
+    private static String CURRENT_ASSETS_TOTAL_PATH = BALANCE_SHEET_PATH + ".current_assets.total";
 
     public Errors validatePreviousPeriod(
         @Valid PreviousPeriod previousPeriod) {
@@ -23,9 +25,29 @@ public class PreviousPeriodValidator extends BaseValidator {
         if (previousPeriod.getBalanceSheet() != null) {
 
             validateTotalFixedAssets(previousPeriod, errors);
+            validateTotalCurrentAssets(previousPeriod, errors);
         }
 
         return errors;
+    }
+
+    public void validateTotalCurrentAssets(PreviousPeriod previousPeriod, Errors errors) {
+
+        CurrentAssets currentAssets = previousPeriod.getBalanceSheet().getCurrentAssets();
+
+        Long stocks = currentAssets.getStocks();
+        Long debtors = currentAssets.getDebtors();
+        Long cashAtBandAndInHand = currentAssets.getCashAtBankAndInHand();
+        Long currentAssetsTotal = currentAssets.getTotalCurrentAssets();
+
+        Long calculatedTotal = 0L;
+
+        calculatedTotal += stocks == null ? 0L : stocks;
+        calculatedTotal += debtors == null ? 0L : debtors;
+        calculatedTotal += cashAtBandAndInHand == null ? 0L : cashAtBandAndInHand;
+
+        validateAggregateTotal(currentAssetsTotal, calculatedTotal, CURRENT_ASSETS_TOTAL_PATH, errors);
+
     }
 
     public void validateTotalFixedAssets(@Valid PreviousPeriod previousPeriod, Errors errors) {
@@ -38,7 +60,7 @@ public class PreviousPeriodValidator extends BaseValidator {
             // Will calculate the total of all fixedAssets fields as they are added to the balance sheet
             Long calculatedTotal = tangible;
 
-            validateAggregateTotal(fixedAssetsTotal, calculatedTotal, TOTAL_PATH, errors);
+            validateAggregateTotal(fixedAssetsTotal, calculatedTotal, FIXED_ASSETS_TOTAL_PATH, errors);
         }
     }
 }
