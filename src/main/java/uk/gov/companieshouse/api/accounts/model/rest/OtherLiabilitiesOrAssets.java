aa10b4eaea2a0a4fe0a93c79ce822e66bac32037
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-11-01 15:11:25 +0000

    add @JsonInclude(Include.NON_NULL) on balance sheet classes to avoid null/empty fields being written to mongo

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/OtherLiabilitiesOrAssets.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/OtherLiabilitiesOrAssets.java
@@ -1,9 +1,11 @@
 package uk.gov.companieshouse.api.accounts.model.rest;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Range;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OtherLiabilitiesOrAssets {
 
     private static final int MAX_RANGE = 99999999;
