8fcde6eb999357b60290fbb8189c9f748dc11fef
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-11-01 09:26:04 +0000

    Create capital and reserve rest and entity models

---
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/entity/CapitalAndReservesEntity.java
@@ -0,0 +1,61 @@
+package uk.gov.companieshouse.api.accounts.model.entity;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+
+public class CapitalAndReservesEntity {
+
+    @Field("called_up_share_capital")
+    private Long calledUpShareCapital;
+
+    @Field("other_reserves")
+    private Long otherReserves;
+
+    @Field("profit_and_loss")
+    private Long profitAndLoss;
+
+    @Field("share_premium_account")
+    private Long sharePremiumAccount;
+
+    @Field("total_shareholders_fund")
+    private Long total;
+
+    public Long getCalledUpShareCapital() {
+        return calledUpShareCapital;
+    }
+
+    public void setCalledUpShareCapital(Long calledUpShareCapital) {
+        this.calledUpShareCapital = calledUpShareCapital;
+    }
+
+    public Long getOtherReserves() {
+        return otherReserves;
+    }
+
+    public void setOtherReserves(Long otherReserves) {
+        this.otherReserves = otherReserves;
+    }
+
+    public Long getProfitAndLoss() {
+        return profitAndLoss;
+    }
+
+    public void setProfitAndLoss(Long profitAndLoss) {
+        this.profitAndLoss = profitAndLoss;
+    }
+
+    public Long getSharePremiumAccount() {
+        return sharePremiumAccount;
+    }
+
+    public void setSharePremiumAccount(Long sharePremiumAccount) {
+        this.sharePremiumAccount = sharePremiumAccount;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+}
---
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/CapitalAndReserves.java
@@ -0,0 +1,61 @@
+package uk.gov.companieshouse.api.accounts.model.rest;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CapitalAndReserves {
+
+    @JsonProperty("called_up_share_capital")
+    private Long calledUpShareCapital;
+
+    @JsonProperty("other_reserves")
+    private Long otherReserves;
+
+    @JsonProperty("profit_and_loss")
+    private Long profitAndLoss;
+
+    @JsonProperty("share_premium_account")
+    private Long sharePremiumAccount;
+
+    @JsonProperty("total_shareholder_funds")
+    private Long total;
+
+    public Long getCalledUpShareCapital() {
+        return calledUpShareCapital;
+    }
+
+    public void setCalledUpShareCapital(Long calledUpShareCapital) {
+        this.calledUpShareCapital = calledUpShareCapital;
+    }
+
+    public Long getOtherReserves() {
+        return otherReserves;
+    }
+
+    public void setOtherReserves(Long otherReserves) {
+        this.otherReserves = otherReserves;
+    }
+
+    public Long getProfitAndLoss() {
+        return profitAndLoss;
+    }
+
+    public void setProfitAndLoss(Long profitAndLoss) {
+        this.profitAndLoss = profitAndLoss;
+    }
+
+    public Long getSharePremiumAccount() {
+        return sharePremiumAccount;
+    }
+
+    public void setSharePremiumAccount(Long sharePremiumAccount) {
+        this.sharePremiumAccount = sharePremiumAccount;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+}
