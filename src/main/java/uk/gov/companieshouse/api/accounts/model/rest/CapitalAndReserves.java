f00dac8ad2955781b66d93f88ccb2949274b8937
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-11-05 14:50:27 +0000

    Add spring validation annotations to capital and reserves fields

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/CapitalAndReserves.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/CapitalAndReserves.java
@@ -2,22 +2,34 @@ package uk.gov.companieshouse.api.accounts.model.rest;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotNull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CapitalAndReserves {
 
+    private static final int MAX_RANGE = 99999999;
+    private static final int MIN_RANGE = 0;
+    private static final int MIN_RANGE_NEGATIVE = -99999999;
+
+    @Range(min=MIN_RANGE,max=MAX_RANGE, message = "value.outside.range")
     @JsonProperty("called_up_share_capital")
     private Long calledUpShareCapital;
 
+    @Range(min=MIN_RANGE_NEGATIVE,max=MAX_RANGE, message = "value.outside.range")
     @JsonProperty("other_reserves")
     private Long otherReserves;
 
+    @Range(min=MIN_RANGE_NEGATIVE,max=MAX_RANGE, message = "value.outside.range")
     @JsonProperty("profit_and_loss")
     private Long profitAndLoss;
 
+    @Range(min=MIN_RANGE,max=MAX_RANGE, message = "value.outside.range")
     @JsonProperty("share_premium_account")
     private Long sharePremiumAccount;
 
+    @NotNull
     @JsonProperty("total_shareholder_funds")
     private Long totalShareholderFunds;
 
