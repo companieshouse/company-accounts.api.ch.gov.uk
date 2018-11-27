c7b4d107debdea3c70675221c02346366a79cc8b
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-11-27 15:33:17 +0000

    Add max length validation to debtors details field

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/Debtors.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/Debtors.java
@@ -2,10 +2,18 @@ package uk.gov.companieshouse.api.accounts.model.rest;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import uk.gov.companieshouse.api.accounts.validation.CharSetValid;
+import uk.gov.companieshouse.charset.CharSet;
+
+import javax.validation.constraints.Size;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Debtors extends RestObject {
 
+    private static final int MAX_FIELD_LENGTH = 20000;
+
+    @Size(max = MAX_FIELD_LENGTH, message = "max.length.exceeded")
+    @CharSetValid(CharSet.CHARACTER_SET_3)
     @JsonProperty("details")
     private String details;
 
