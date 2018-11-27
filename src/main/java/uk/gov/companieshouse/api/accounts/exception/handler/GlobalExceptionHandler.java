0effe57d41b52ae50454afa0e05727b3d8867e7d
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-11-27 17:15:42 +0000

    removed unused import

--- src/main/java/uk/gov/companieshouse/api/accounts/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/exception/handler/GlobalExceptionHandler.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
