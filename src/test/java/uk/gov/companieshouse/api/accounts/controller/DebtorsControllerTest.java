c4b1b8c5ecba79a31e72b627bf79b3a7367a3b05
caismith-ch <csmith2@companieshouse.gov.uk>
2018-12-03 10:46:08 +0000

    Add tests for updating debtors to DebtorsControllerTest

--- src/test/java/uk/gov/companieshouse/api/accounts/controller/DebtorsControllerTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/controller/DebtorsControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import uk.gov.companieshouse.api.accounts.AttributeName;
 import uk.gov.companieshouse.api.accounts.exception.DataException;
+import uk.gov.companieshouse.api.accounts.links.SmallFullLinkType;
+import uk.gov.companieshouse.api.accounts.model.rest.SmallFull;
 import uk.gov.companieshouse.api.accounts.model.rest.notes.Debtors.Debtors;
 import uk.gov.companieshouse.api.accounts.model.validation.Errors;
 import uk.gov.companieshouse.api.accounts.service.impl.DebtorsService;
@@ -23,9 +25,12 @@ import uk.gov.companieshouse.api.accounts.utility.ErrorMapper;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,6 +54,12 @@ public class DebtorsControllerTest {
 
     @Mock
     DebtorsService debtorsService;
+
+    @Mock
+    private SmallFull smallFull;
+
+    @Mock
+    private Map<String, String> smallFullLinks;
 
     @Mock
     private ErrorMapper errorMapper;
@@ -119,5 +130,89 @@ public class DebtorsControllerTest {
 
         assertNotNull(responseEntity);
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Update debtors - no small full link")
+    void updateDebtorsNoSmallFullLink() {
+
+        when(request.getAttribute(AttributeName.SMALLFULL.getValue())).thenReturn(smallFull);
+        when(smallFull.getLinks()).thenReturn(smallFullLinks);
+        when(smallFullLinks.get(SmallFullLinkType.DEBTORS_NOTE.getLink())).thenReturn(null);
+
+        ResponseEntity responseEntity =
+                controller.update(debtors, bindingResult, COMPANY_ACCOUNTS_ID, request);
+
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Update debtors - has binding errors")
+    void updateDebtorsBindingErrors() {
+
+        when(request.getAttribute(AttributeName.SMALLFULL.getValue())).thenReturn(smallFull);
+        when(smallFull.getLinks()).thenReturn(smallFullLinks);
+        when(smallFullLinks.get(SmallFullLinkType.DEBTORS_NOTE.getLink())).thenReturn("");
+        when(bindingResult.hasErrors()).thenReturn(true);
+        when(errorMapper.mapBindingResultErrorsToErrorModel(bindingResult)).thenReturn(new Errors());
+
+        ResponseEntity responseEntity =
+                controller.update(debtors, bindingResult, COMPANY_ACCOUNTS_ID, request);
+
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Update debtors - success")
+    void updateAccountingPoliciesSuccess() throws DataException {
+
+        when(request.getAttribute(anyString())).thenReturn(smallFull).thenReturn(transaction);
+        when(smallFull.getLinks()).thenReturn(smallFullLinks);
+        when(smallFullLinks.get(SmallFullLinkType.DEBTORS_NOTE.getLink())).thenReturn("");
+        when(bindingResult.hasErrors()).thenReturn(false);
+
+        ResponseObject responseObject = new ResponseObject(ResponseStatus.UPDATED,
+                debtors);
+        when(debtorsService.update(debtors, transaction, COMPANY_ACCOUNTS_ID, request))
+                .thenReturn(responseObject);
+
+        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        when(apiResponseMapper.map(responseObject.getStatus(), responseObject.getData(), responseObject.getErrors()))
+                .thenReturn(responseEntity);
+
+        ResponseEntity returnedResponse =
+                controller.update(debtors, bindingResult, COMPANY_ACCOUNTS_ID, request);
+
+        assertNotNull(returnedResponse);
+        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Update debtors - data exception thrown")
+    void updateAccountingPoliciesDataException() throws DataException {
+
+        when(request.getAttribute(anyString())).thenReturn(smallFull).thenReturn(transaction);
+        when(smallFull.getLinks()).thenReturn(smallFullLinks);
+        when(smallFullLinks.get(SmallFullLinkType.DEBTORS_NOTE.getLink())).thenReturn("");
+        when(bindingResult.hasErrors()).thenReturn(false);
+
+        DataException dataException = new DataException("");
+        when(debtorsService.update(debtors, transaction, COMPANY_ACCOUNTS_ID, request))
+                .thenThrow(dataException);
+
+        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        when(apiResponseMapper.map(dataException)).thenReturn(responseEntity);
+
+        ResponseEntity returnedResponse =
+                controller.update(debtors, bindingResult, COMPANY_ACCOUNTS_ID, request);
+
+        assertNotNull(returnedResponse);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
     }
 }
