b7f5a552cf761cc6b2a4d863a722ff75b77109ed
sroberts2 <sroberts2@companieshouse.gov.uk>
2019-03-12 13:46:44 +0000

    refactored code

--- src/main/java/uk/gov/companieshouse/api/accounts/interceptor/TransactionInterceptor.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/interceptor/TransactionInterceptor.java
@@ -3,17 +3,14 @@ package uk.gov.companieshouse.api.accounts.interceptor;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
 import uk.gov.companieshouse.api.ApiClient;
 import uk.gov.companieshouse.api.accounts.AttributeName;
 import uk.gov.companieshouse.api.accounts.CompanyAccountsApplication;
@@ -28,62 +25,57 @@ import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 @Component
 public class TransactionInterceptor extends HandlerInterceptorAdapter {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CompanyAccountsApplication.APPLICATION_NAME_SPACE);
+    private static final Logger LOGGER = LoggerFactory
+        .getLogger(CompanyAccountsApplication.APPLICATION_NAME_SPACE);
 
     @Autowired
     private ApiClientService apiClientService;
 
     /**
-     * Pre handle method to validate the request before it reaches the
-     * controller. Check if the url has an existing transaction and save it in
-     * the request's attribute. If transaction is not found then return 404
+     * Pre handle method to validate the request before it reaches the controller. Check if the url
+     * has an existing transaction and save it in the request's attribute. If transaction is not
+     * found then return 404
      */
     @Override
     @SuppressWarnings("unchecked")
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) {
 
         final Map<String, Object> debugMap = new HashMap<>();
         debugMap.put("request_method", request.getMethod());
 
         try {
             Map<String, String> pathVariables = (Map) request
-                    .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+                .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
 
             String transactionId = pathVariables.get("transactionId");
-            String passthroughHeader = request.getHeader(ApiSdkManager.getEricPassthroughTokenHeader());
+            String passthroughHeader = request
+                .getHeader(ApiSdkManager.getEricPassthroughTokenHeader());
 
-            ApiClient apiClient;
-            try {
-                apiClient = apiClientService.getApiClient(passthroughHeader);
-            } catch (IOException e) {
+            ApiClient apiClient = apiClientService.getApiClient(passthroughHeader);
 
-                LOGGER.errorRequest(request, e, debugMap);
-                response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-                return false;
-            }
-
-            Transaction transaction;
-            try {
-                transaction = apiClient.transactions().get("/transactions/" + transactionId).execute();
-            } catch (ApiErrorResponseException e) {
-
-                LOGGER.errorRequest(request, e, debugMap);
-                response.setStatus(e.getStatusCode());
-                return false;
-            } catch (URIValidationException e) {
-
-                LOGGER.errorRequest(request, e, debugMap);
-                response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-                return false;
-            }
+            Transaction transaction = apiClient.transactions().get("/transactions/" + transactionId)
+                .execute();
 
             request.setAttribute(AttributeName.TRANSACTION.getValue(), transaction);
-
             return true;
+
         } catch (HttpClientErrorException e) {
 
             LOGGER.errorRequest(request, e, debugMap);
             response.setStatus(e.getStatusCode().value());
+            return false;
+
+        } catch (ApiErrorResponseException e) {
+
+            LOGGER.errorRequest(request, e, debugMap);
+            response.setStatus(e.getStatusCode());
+            return false;
+
+        } catch (URIValidationException | IOException e) {
+
+            LOGGER.errorRequest(request, e, debugMap);
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
             return false;
         }
     }
