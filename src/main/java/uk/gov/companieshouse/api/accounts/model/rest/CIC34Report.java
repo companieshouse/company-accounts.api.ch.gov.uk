6b5f05277fcfb2f4a01c7aa1d94987b401eda2e6
bsaunders95 <bsaunders@companieshouse.gov.uk>
2019-04-03 16:28:09 +0100

    Add validation to the CIC34 report model

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/CIC34Report.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/CIC34Report.java
@@ -3,19 +3,35 @@ package uk.gov.companieshouse.api.accounts.model.rest;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import uk.gov.companieshouse.api.accounts.validation.CharSetValid;
+import uk.gov.companieshouse.charset.CharSet;
 
 @JsonInclude(Include.NON_NULL)
 public class CIC34Report extends RestObject {
 
+    private static final int MAX_FIELD_LENGTH = 20000;
+    private static final int MIN_FIELD_LENGTH = 1;
+
+    @NotBlank
+    @Size(max = MAX_FIELD_LENGTH, message = "max.length.exceeded")
+    @CharSetValid(CharSet.CHARACTER_SET_3)
     @JsonProperty("company_activities_and_impact")
     private String companyActivitiesAndImpact;
 
+    @Size(min = MIN_FIELD_LENGTH, max = MAX_FIELD_LENGTH, message="invalid.input.length")
+    @CharSetValid(CharSet.CHARACTER_SET_3)
     @JsonProperty("consultation_with_stakeholders")
     private String consultationWithStakeholders;
 
+    @Size(min = MIN_FIELD_LENGTH, max = MAX_FIELD_LENGTH, message="invalid.input.length")
+    @CharSetValid(CharSet.CHARACTER_SET_3)
     @JsonProperty("directors_remuneration")
     private String directorsRemuneration;
 
+    @Size(min = MIN_FIELD_LENGTH, max = MAX_FIELD_LENGTH, message="invalid.input.length")
+    @CharSetValid(CharSet.CHARACTER_SET_3)
     @JsonProperty("transfer_of_assets")
     private String transferOfAssets;
 
