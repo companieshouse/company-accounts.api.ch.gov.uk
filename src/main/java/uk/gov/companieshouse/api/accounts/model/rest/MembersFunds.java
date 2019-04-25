2619dd2b80694a18760960ba7e5fde115ebf4edd
bsaunders95 <bsaunders@companieshouse.gov.uk>
2019-04-25 12:46:11 +0100

    Enforce field level constraint validation on members' funds

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/MembersFunds.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/MembersFunds.java
@@ -3,13 +3,20 @@ package uk.gov.companieshouse.api.accounts.model.rest;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Range;
 
 @JsonInclude(Include.NON_NULL)
 public class MembersFunds {
 
+    private static final int MIN_RANGE = -99999999;
+    private static final int MAX_RANGE = 99999999;
+
+    @Range(min = MIN_RANGE, max = MAX_RANGE, message = "value.outside.range")
     @JsonProperty("profit_and_loss_account")
     private Long profitAndLossAccount;
 
+    @NotNull
     @JsonProperty("total_members_funds")
     private Long totalMembersFunds;
 
