9d5c709a03f2f14026f4229a245c62445011b772
Jamie87 <jwillison@companieshouse.gov.uk>
2019-10-18 13:02:44 +0100

    start unit tests

--- src/test/java/uk/gov/companieshouse/api/accounts/service/impl/ProfitLossServiceTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/service/impl/ProfitLossServiceTest.java
@@ -1,4 +1,73 @@
 package uk.gov.companieshouse.api.accounts.service.impl;
 
+import com.mongodb.MongoException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DuplicateKeyException;
+import uk.gov.companieshouse.api.accounts.ResourceName;
+import uk.gov.companieshouse.api.accounts.exception.DataException;
+import uk.gov.companieshouse.api.accounts.model.entity.profitloss.ProfitLossEntity;
+import uk.gov.companieshouse.api.accounts.model.rest.profitloss.ProfitLoss;
+import uk.gov.companieshouse.api.accounts.repository.ProfitLossRepository;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
+import uk.gov.companieshouse.api.accounts.transformer.ProfitLossTransformer;
+import uk.gov.companieshouse.api.accounts.utility.impl.KeyIdGenerator;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.api.model.transaction.TransactionLinks;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProfitLossServiceTest {
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private Transaction transaction;
+
+    @Mock
+    private TransactionLinks transactionLinks;
+
+    @Mock
+    private SmallFullService smallFullService;
+
+    @Mock
+    private DuplicateKeyException duplicateKeyException;
+
+    @Mock
+    private MongoException mongoException;
+
+    @Mock
+    private KeyIdGenerator keyIdGenerator;
+
+    @Mock
+    private ProfitLoss profitLoss;
+
+    @Mock
+    private ProfitLossRepository profitLossRepository;
+
+    @Mock
+    private ProfitLossEntity profitLossEntity;
+
+    @Mock
+    private ProfitLossService profitLossService;
+
+    @Mock
+    private ProfitLossTransformer profitLossTransformer;
+
+    private static final String SELF_LINK = "self_link";
+    private static final String COMPANY_ACCOUNTS_ID = "companyAccountsId";
+    private static final String RESOURCE_ID = "resourceId";
+
 }
