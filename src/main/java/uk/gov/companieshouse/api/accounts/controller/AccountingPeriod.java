b296f12f276730b5e303942adcb91bab3ce9c090
Faisal Amin <famin@companieshouse.gov.uk>
2019-10-24 13:32:14 +0100

    renamed Period to AccountingPeriod

--- src/main/java/uk/gov/companieshouse/api/accounts/controller/AccountingPeriod.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/controller/AccountingPeriod.java
@@ -2,20 +2,20 @@ package uk.gov.companieshouse.api.accounts.controller;
 
 import java.util.Arrays;
 
-public enum Period {
+public enum AccountingPeriod {
     CURRENT_PERIOD("current-period"), PREVIOUS_PERIOD("previous-period");
 
-    private String period;
+    private String accountingPeriod;
 
-    Period(String s) {
-        this.period = s;
+    AccountingPeriod(String s) {
+        this.accountingPeriod = s;
     }
 
     private String getPeriod() {
-        return period;
+        return accountingPeriod;
     }
 
-    public static Period fromString(String period) {
+    public static AccountingPeriod fromString(String period) {
         return Arrays.stream(values())
                 .filter(periodEnum -> periodEnum.getPeriod().equals(period))
                 .findFirst()
