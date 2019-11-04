7207548d86685c2fd23e9dc039f3f7f5405b95b7
bsaunders95 <bsaunders@companieshouse.gov.uk>
2019-11-04 14:05:09 +0000

    Apply range constraints to profit and loss fields

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/profitloss/ProfitOrLossForFinancialYear.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/profitloss/ProfitOrLossForFinancialYear.java
@@ -2,13 +2,19 @@ package uk.gov.companieshouse.api.accounts.model.rest.profitloss;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Range;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProfitOrLossForFinancialYear {
 
+    private static final int MAX_RANGE = 99999999;
+    private static final int MIN_RANGE = -99999999;
+
+    @Range(min = MIN_RANGE, max = MAX_RANGE, message = "value.outside.range")
     @JsonProperty("tax")
     private Long tax;
 
+    @Range(min = MIN_RANGE, max = MAX_RANGE, message = "value.outside.range")
     @JsonProperty("total_profit_or_loss_for_financial_year")
     private Long totalProfitOrLossForFinancialYear;
 
