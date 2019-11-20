a0a856238858e46989b60425b2e60667e01028ba
divya051988 <dparameswaran@companieshouse.gov.uk>
2019-11-20 14:13:45 +0000

    Add Statement Service test

---
+++ src/test/java/uk/gov/companieshouse/api/accounts/service/impl/StatementsServicePropertiesTest.java
@@ -0,0 +1,43 @@
+package uk.gov.companieshouse.api.accounts.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class StatementsServicePropertiesTest {
+
+    private StatementsServiceProperties statementsServiceProperties;
+
+    @BeforeEach
+    void setup() {
+        statementsServiceProperties = new StatementsServiceProperties();
+    }
+
+    @Test
+    @DisplayName("Test Clone of Statements ")
+    void testGetCloneOfStatements () {
+        statementsServiceProperties.setStatements(new HashMap<>());
+        assertNotNull(statementsServiceProperties.getCloneOfStatements());
+    }
+
+    @Test
+    @DisplayName("Test annotations on the class")
+    void testAnnotations (){
+        assertTrue(this.statementsServiceProperties.getClass().isAnnotationPresent(ConfigurationProperties.class));
+        assertTrue(this.statementsServiceProperties.getClass().isAnnotationPresent(Component.class));
+        assertTrue(this.statementsServiceProperties.getClass().isAnnotationPresent(PropertySource.class));
+    }
+}
