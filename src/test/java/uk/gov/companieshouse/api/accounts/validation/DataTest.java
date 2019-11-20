66c60010683c07c17aeaa75806d2b2e3b6df8704
divya051988 <dparameswaran@companieshouse.gov.uk>
2019-11-20 10:37:54 +0000

    Add annotation tests to data test calss

--- src/test/java/uk/gov/companieshouse/api/accounts/validation/DataTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/validation/DataTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.lang.reflect.Method;
+import org.junit.Assert;
+
+import javax.xml.bind.annotation.XmlElement;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -38,6 +42,18 @@ public class DataTest {
     @DisplayName("Test Get CompaniesHouseRegisteredNumber")
     void testGetCompaniesHouseRegisteredNumber() {
         assertEquals("CompaniesHouseRegisteredNumber",data.getCompaniesHouseRegisteredNumber());
+    }
+
+    @Test
+    @DisplayName("Test If Get Methods have the appropriate annotation")
+    public void testHasMethodsWithAnnotation() {
+        Class dataClass = Data.class;
+        Method[] methods = dataClass.getDeclaredMethods();
+        for (Method m : methods) {
+            if(m.getName().startsWith("get")) {
+            Assert.assertNotNull("Method :"+m.getName() + " does not have annotation XmlElement",m.getAnnotation(XmlElement.class));
+            }
+        }
     }
 
 }
