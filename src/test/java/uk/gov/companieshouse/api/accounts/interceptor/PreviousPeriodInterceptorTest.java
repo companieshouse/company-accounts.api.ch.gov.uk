fdeb2a2a0b098145ddfd88f98915b10be8876bff
Faisal Amin <famin@companieshouse.gov.uk>
2020-01-21 18:11:04 +0000

    Current and Previous period interceptor test code coverage

--- src/test/java/uk/gov/companieshouse/api/accounts/interceptor/PreviousPeriodInterceptorTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/interceptor/PreviousPeriodInterceptorTest.java
@@ -1,6 +1,5 @@
 package uk.gov.companieshouse.api.accounts.interceptor;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -26,6 +25,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
@@ -39,16 +39,15 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
   class PreviousPeriodInterceptorTest {
 
+
     @Mock
     private PreviousPeriod previousPeriod;
 
     @Mock
     private ResponseObject responseObject;
 
-    @Mock
     private Transaction transaction;
 
-    @Mock
     private SmallFull smallFull;
 
     @Mock
@@ -73,13 +72,17 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
     private static final String COMPANY_ACCOUNT_ID = "companyAccountId";
     private  static final String X_REQUEST_ID = "X-Request-Id";
     private static final String LINK_PREVIOUS_PERIOD = "linkToPreviousPeriod";
+    private static final String NO_LINK_PREVIOUS_PERIOD = "noLinkToPreviousPeriod";
+    private static final String URI = "../../../previous-period";
 
-    @BeforeEach
     void setUp() throws NoSuchAlgorithmException {
 
         Map<String, String> pathVariables = new HashMap<>();
         pathVariables.put(TRANSACTION_ID, "5555");
         pathVariables.put(COMPANY_ACCOUNT_ID, "test");
+
+        smallFull = mock(SmallFull.class);
+        transaction = mock(Transaction.class);
 
         when(httpServletRequest.getHeader(X_REQUEST_ID)).thenReturn("test");
         doReturn(transaction).when(httpServletRequest).getAttribute(AttributeName.TRANSACTION.getValue());
@@ -87,11 +90,14 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
         doReturn(pathVariables).when(httpServletRequest).getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
 
         when(httpServletRequest.getMethod()).thenReturn("GET");
+
     }
 
     @Test
     @DisplayName("Tests the interceptor returns correctly when all is valid")
     void testReturnsCorrectlyOnValidConditions() throws NoSuchAlgorithmException, DataException {
+
+        setUp();
         when(previousPeriodService.find(anyString(), any(HttpServletRequest.class))).thenReturn(responseObject);
         when(responseObject.getStatus()).thenReturn(ResponseStatus.FOUND);
         when(responseObject.getData()).thenReturn(previousPeriod);
@@ -109,9 +115,87 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
     @Test
     @DisplayName("Tests the interceptor returns false on a failed PreviousPeriodEntity lookup")
     void testReturnsFalseForAFailedLookup() throws NoSuchAlgorithmException, DataException {
+        setUp();
         doThrow(mock(DataException.class)).when(previousPeriodService).find(anyString(), any(HttpServletRequest.class));
         assertFalse(previousPeriodInterceptor.preHandle(httpServletRequest, httpServletResponse,
                 new Object()));
     }
 
+    @Test
+    @DisplayName(("Test the interceptor returns true when URI ends with previous-period"))
+    void testReturnTrueForURIEndsWithPreviousPeriod() throws NoSuchAlgorithmException {
+
+        when(httpServletRequest.getMethod()).thenReturn("POST");
+        when (httpServletRequest.getRequestURI()).thenReturn(URI);
+
+        previousPeriodInterceptor.preHandle(httpServletRequest, httpServletResponse,
+                new Object());
+
+         assertTrue(httpServletRequest.getMethod().equalsIgnoreCase("POST") && httpServletRequest.getRequestURI()
+                .endsWith("previous-period"));
+    }
+
+    @Test
+    @DisplayName(("Test the interceptor when transaction is null"))
+    void testTransactionIsNull() throws NoSuchAlgorithmException {
+
+        when(httpServletRequest.getMethod()).thenReturn("GET");
+        doReturn(null).when(httpServletRequest).getAttribute(AttributeName.TRANSACTION.getValue());
+        previousPeriodInterceptor.preHandle(httpServletRequest, httpServletResponse,
+                new Object());
+
+        verify(httpServletResponse).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    @DisplayName(("Test the interceptor when smallFull is null"))
+    void testSmallFullIsNull() throws NoSuchAlgorithmException {
+
+        transaction = mock(Transaction.class);
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put(TRANSACTION_ID, "5555");
+        pathVariables.put(COMPANY_ACCOUNT_ID, "test");
+
+        when(httpServletRequest.getMethod()).thenReturn("GET");
+        doReturn(pathVariables).when(httpServletRequest).getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        doReturn(transaction).when(httpServletRequest).getAttribute(AttributeName.TRANSACTION.getValue());
+
+        previousPeriodInterceptor.preHandle(httpServletRequest, httpServletResponse,
+                new Object());
+
+        verify(httpServletResponse).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    @DisplayName("Previous Period interceptor - returns not found status")
+    void previousPeriodInterceptorReturnsNotFoundStatus() throws DataException, NoSuchAlgorithmException {
+
+        setUp();
+        when(previousPeriodService.find(anyString(), any(HttpServletRequest.class))).thenReturn(responseObject);
+        when(responseObject.getStatus()).thenReturn(ResponseStatus.NOT_FOUND);
+
+        previousPeriodInterceptor.preHandle(httpServletRequest, httpServletResponse,
+                new Object());
+
+        verify(httpServletResponse).setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("Previous Period interceptor - returns bad request status")
+    void previousPeriodInterceptorReturnsBadRequestStatus() throws DataException, NoSuchAlgorithmException {
+
+        setUp();
+        when(previousPeriodService.find(anyString(), any(HttpServletRequest.class))).thenReturn(responseObject);
+        when(responseObject.getStatus()).thenReturn(ResponseStatus.FOUND);
+        when(responseObject.getData()).thenReturn(previousPeriod);
+        when(smallFull.getLinks()).thenReturn(smallFullLinks);
+        when(previousPeriod.getLinks()).thenReturn(previousPeriodLinks);
+        when(previousPeriodLinks.get("self")).thenReturn(LINK_PREVIOUS_PERIOD);
+        when(smallFullLinks.get(SmallFullLinkType.PREVIOUS_PERIOD.getLink())).thenReturn(NO_LINK_PREVIOUS_PERIOD);
+
+        previousPeriodInterceptor.preHandle(httpServletRequest, httpServletResponse,
+                new Object());
+
+        verify(httpServletResponse).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    }
 }
