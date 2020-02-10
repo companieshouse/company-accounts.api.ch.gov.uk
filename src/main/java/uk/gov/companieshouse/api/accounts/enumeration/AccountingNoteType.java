01ebd9857aff9e96363d285eef755738bea52acc
bsaunders95 <bsaunders@companieshouse.gov.uk>
2020-02-10 19:39:49 +0000

    Expand range of enums

--- src/main/java/uk/gov/companieshouse/api/accounts/enumeration/AccountingNoteType.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/enumeration/AccountingNoteType.java
@@ -5,10 +5,66 @@ import uk.gov.companieshouse.api.accounts.links.SmallFullLinkType;
 
 public enum AccountingNoteType {
 
-    SMALL_FULL_STOCKS(AccountType.SMALL_FULL, NoteType.STOCKS, SmallFullLinkType.STOCKS_NOTE,
-                      "small-full-accounts#stocks-note", true),
-    SMALL_FULL_DEBTORS(AccountType.SMALL_FULL, NoteType.DEBTORS, SmallFullLinkType.DEBTORS_NOTE,
-                      "small-full-accounts-note#debtors", true);
+    SMALL_FULL_ACCOUNTING_POLICIES(
+                    AccountType.SMALL_FULL,
+                    NoteType.ACCOUNTING_POLICIES,
+                    SmallFullLinkType.ACCOUNTING_POLICY_NOTE,
+                    "small-full-accounts-note#accounting-policy",
+                    false),
+    SMALL_FULL_EMPLOYEES(
+                    AccountType.SMALL_FULL,
+                    NoteType.EMPLOYEES,
+                    SmallFullLinkType.EMPLOYEES_NOTE,
+                    "small-full-accounts-note#employees",
+                    true),
+    SMALL_FULL_INTANGIBLE_ASSETS(
+                    AccountType.SMALL_FULL,
+                    NoteType.INTANGIBLE_ASSETS,
+                    SmallFullLinkType.INTANGIBLE_ASSETS_NOTE,
+                    "small-full-accounts-note#intangible-assets",
+                    true),
+    SMALL_FULL_TANGIBLE_ASSETS(
+                    AccountType.SMALL_FULL,
+                    NoteType.TANGIBLE_ASSETS,
+                    SmallFullLinkType.TANGIBLE_ASSETS_NOTE,
+                    "small-full-accounts-note#tangible-assets",
+                    true),
+    SMALL_FULL_FIXED_ASSETS_INVESTMENTS(
+                    AccountType.SMALL_FULL,
+                    NoteType.FIXED_ASSETS_INVESTMENTS,
+                    SmallFullLinkType.FIXED_ASSETS_INVESTMENTS_NOTE,
+                    "small-full-accounts-note#fixed-assets-investments",
+                    true),
+    SMALL_FULL_STOCKS(
+                    AccountType.SMALL_FULL,
+                    NoteType.STOCKS,
+                    SmallFullLinkType.STOCKS_NOTE,
+                    "small-full-accounts#stocks-note",
+                    true),
+    SMALL_FULL_DEBTORS(
+                    AccountType.SMALL_FULL,
+                    NoteType.DEBTORS,
+                    SmallFullLinkType.DEBTORS_NOTE,
+                    "small-full-accounts-note#debtors",
+                    true),
+    SMALL_FULL_CURRENT_ASSETS_INVESTMENTS(
+                    AccountType.SMALL_FULL,
+                    NoteType.CURRENT_ASSETS_INVESTMENTS,
+                    SmallFullLinkType.CURRENT_ASSETS_INVESTMENTS_NOTE,
+                    "small-full-accounts-note#current-assets-investments",
+                    true),
+    SMALL_FULL_CREDITORS_AFTER(
+                    AccountType.SMALL_FULL,
+                    NoteType.CREDITORS_AFTER,
+                    SmallFullLinkType.CREDITORS_AFTER_MORE_THAN_ONE_YEAR_NOTE,
+                    "small-full-accounts-note#creditors-after-one-year",
+                    true),
+    SMALL_FULL_CREDITORS_WITHIN(
+                    AccountType.SMALL_FULL,
+                    NoteType.CREDITORS_WITHIN,
+                    SmallFullLinkType.CREDITORS_WITHIN_ONE_YEAR_NOTE,
+                    "small-full-accounts-note#creditors-within-one-year",
+                    true);
 
     private AccountType accountType;
     private NoteType noteType;
--- src/main/java/uk/gov/companieshouse/api/accounts/enumeration/NoteType.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/enumeration/NoteType.java
@@ -2,8 +2,16 @@ package uk.gov.companieshouse.api.accounts.enumeration;
 
 public enum NoteType {
 
+    ACCOUNTING_POLICIES("accounting-policies"),
+    EMPLOYEES("employees"),
+    INTANGIBLE_ASSETS("intangible-assets"),
+    TANGIBLE_ASSETS("tangible-assets"),
+    FIXED_ASSETS_INVESTMENTS("fixed-assets-investments"),
     STOCKS("stocks"),
-    DEBTORS("debtors");
+    DEBTORS("debtors"),
+    CURRENT_ASSETS_INVESTMENTS("current-assets-investments"),
+    CREDITORS_AFTER("creditors-after-more-than-one-year"),
+    CREDITORS_WITHIN("creditors-within-one-year");
 
     NoteType(String type) {
         this.type = type;
