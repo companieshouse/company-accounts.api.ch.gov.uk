b4e6eac385a10b7988b2cfefc16d0b1cb986afb7
Rafa <rgarcia@companieshouse.gov.uk>
2020-03-27 14:26:52 +0000

    Add authentication to create company accounts

--- src/test/java/uk/gov/companieshouse/api/accounts/CompanyAccountsApplicationTest.java
+++ src/test/java/uk/gov/companieshouse/api/accounts/CompanyAccountsApplicationTest.java
@@ -1,19 +1,23 @@
 package uk.gov.companieshouse.api.accounts;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+import uk.gov.companieshouse.api.accounts.interceptor.AuthenticationInterceptor;
 import uk.gov.companieshouse.api.accounts.interceptor.CicReportInterceptor;
 import uk.gov.companieshouse.api.accounts.interceptor.ClosedTransactionInterceptor;
 import uk.gov.companieshouse.api.accounts.interceptor.CompanyAccountInterceptor;
@@ -24,10 +28,7 @@ import uk.gov.companieshouse.api.accounts.interceptor.OpenTransactionInterceptor
 import uk.gov.companieshouse.api.accounts.interceptor.PreviousPeriodInterceptor;
 import uk.gov.companieshouse.api.accounts.interceptor.SmallFullInterceptor;
 import uk.gov.companieshouse.api.accounts.interceptor.TransactionInterceptor;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import uk.gov.companieshouse.api.interceptor.TokenPermissionsInterceptor;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -67,6 +68,12 @@ public class CompanyAccountsApplicationTest {
     private LoggingInterceptor loggingInterceptor;
 
     @Mock
+    private TokenPermissionsInterceptor tokenPermissiosInterceptor;
+
+    @Mock
+    private AuthenticationInterceptor authenticationInterceptor;
+
+    @Mock
     private InterceptorRegistry interceptorRegistry;
 
     @Mock
@@ -75,24 +82,28 @@ public class CompanyAccountsApplicationTest {
     @Test
     @DisplayName("Test if interceptors are added correctly")
     void testAddInterceptors() {
-
-        when(interceptorRegistry.addInterceptor(any())).thenReturn(interceptorRegistration);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(loggingInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(tokenPermissiosInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(authenticationInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(transactionInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(openTransactionInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(closedTransactionInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(companyAccountInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(smallFullInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(currentPeriodInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(previousPeriodInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(cicReportInterceptor);
+        doReturn(interceptorRegistration).when(interceptorRegistry).addInterceptor(directorsReportInterceptor);
         when(interceptorRegistration.addPathPatterns(Mockito.<String>any())).thenReturn(interceptorRegistration);
         when(interceptorRegistration.excludePathPatterns(anyString())).thenReturn(interceptorRegistration);
+
         companyAccountsApplication.addInterceptors(interceptorRegistry);
-        verify(interceptorRegistry, times(1)).addInterceptor(loggingInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(transactionInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(openTransactionInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(closedTransactionInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(companyAccountInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(smallFullInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(currentPeriodInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(previousPeriodInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(cicReportInterceptor);
-        verify(interceptorRegistry, times(1)).addInterceptor(directorsReportInterceptor);
-        verify(interceptorRegistration, times(4)).excludePathPatterns(anyString());
-        verify(interceptorRegistration, times(6)).addPathPatterns(anyString(),anyString());
-        verify(interceptorRegistration, times(3)).addPathPatterns(anyString());
+
+        InOrder inOrder = Mockito.inOrder(interceptorRegistry);
+        inOrder.verify(interceptorRegistry).addInterceptor(tokenPermissiosInterceptor);
+        inOrder.verify(interceptorRegistry).addInterceptor(authenticationInterceptor);
+
+        verifyNoMoreInteractions(interceptorRegistry);
     }
 
 }
