30ac55bf5f6007bf35137c94de4f61cc3a3c69e3
amoriartyCH <amoriarty@companieshouse.gov.uk>
2020-05-15 11:00:25 +0100

    Change CIC validator to use company profile to fetch accounting dates

--- src/main/java/uk/gov/companieshouse/api/accounts/validation/CicApprovalValidator.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/CicApprovalValidator.java
@@ -2,32 +2,50 @@ package uk.gov.companieshouse.api.accounts.validation;
 
 import java.time.LocalDate;
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.accounts.AttributeName;
+import uk.gov.companieshouse.api.accounts.exception.DataException;
+import uk.gov.companieshouse.api.accounts.exception.ServiceException;
 import uk.gov.companieshouse.api.accounts.model.rest.CicApproval;
 import uk.gov.companieshouse.api.accounts.model.rest.CompanyAccount;
 import uk.gov.companieshouse.api.accounts.model.validation.Error;
 import uk.gov.companieshouse.api.accounts.model.validation.Errors;
+import uk.gov.companieshouse.api.accounts.service.CompanyService;
+import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
 
 @Component
 public class CicApprovalValidator extends BaseValidator {
 
+    @Autowired
+    private CompanyService companyService;
+
     private static final String APPROVAL_PATH = "$.cic_approval";
     private static final String DATE_PATH = APPROVAL_PATH + ".date";
 
-    public Errors validateCicReportApproval(CicApproval cicApproval, HttpServletRequest request) {
+    public Errors validateCicReportApproval(CicApproval cicApproval, HttpServletRequest request) throws DataException {
 
         Errors errors = new Errors();
 
-        CompanyAccount companyAccount = (CompanyAccount) request
-            .getAttribute(AttributeName.COMPANY_ACCOUNT.getValue());
+        Transaction transaction =
+                (Transaction) request.getAttribute(AttributeName.TRANSACTION.getValue());
 
-        LocalDate periodEndDate = companyAccount.getNextAccounts().getPeriodEndOn();
-        LocalDate approvalDate = cicApproval.getDate();
+        try {
+            CompanyProfileApi companyProfile =
+                    companyService.getCompanyProfile(transaction.getCompanyNumber());
 
-        if (approvalDate.isBefore(periodEndDate) || approvalDate.isEqual(periodEndDate)) {
-            errors.addError(new Error(dateInvalid, DATE_PATH, LocationType.JSON_PATH.getValue(),
-                ErrorType.VALIDATION.getType()));
+            LocalDate periodEndDate = companyProfile.getAccounts().getNextAccounts().getPeriodEndOn();
+            LocalDate approvalDate = cicApproval.getDate();
+
+            if (approvalDate.isBefore(periodEndDate) || approvalDate.isEqual(periodEndDate)) {
+                errors.addError(new Error(dateInvalid, DATE_PATH, LocationType.JSON_PATH.getValue(),
+                        ErrorType.VALIDATION.getType()));
+            }
+
+        } catch (ServiceException e) {
+            throw new DataException(e);
         }
 
         return errors;
