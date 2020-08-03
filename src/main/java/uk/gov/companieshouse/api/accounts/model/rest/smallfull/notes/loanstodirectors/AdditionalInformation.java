cd8ad8da436b0c756e58e0386ef394967a6ec484
bsaunders95 <bsaunders@companieshouse.gov.uk>
2020-08-03 14:29:19 +0100

    Implement validation for loans additional info

--- src/main/java/uk/gov/companieshouse/api/accounts/model/rest/smallfull/notes/loanstodirectors/AdditionalInformation.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/model/rest/smallfull/notes/loanstodirectors/AdditionalInformation.java
@@ -2,11 +2,19 @@ package uk.gov.companieshouse.api.accounts.model.rest.smallfull.notes.loanstodir
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.Size;
 import uk.gov.companieshouse.api.accounts.model.rest.RestObject;
+import uk.gov.companieshouse.api.accounts.validation.CharSetValid;
+import uk.gov.companieshouse.charset.CharSet;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AdditionalInformation extends RestObject {
 
+    private static final int MAX_FIELD_LENGTH = 20000;
+    private static final int MIN_FIELD_LENGTH = 1;
+
+    @Size(min = MIN_FIELD_LENGTH, max = MAX_FIELD_LENGTH, message = "invalid.input.length")
+    @CharSetValid(CharSet.CHARACTER_SET_3)
     @JsonProperty("details")
     private String details;
 
