f8258b431d9068329c5d9498f0ae64cf229c4f9b
mreis-ch <mreis@companieshouse.gov.uk>
2020-10-05 14:50:57 +0100

    Add validator for checking previous period on transaction closure
    Create validator PreviousPeriodTnClosureValidator to validate previous period exists on small full account only company is multi year filer.
    Update CurrentPeriodTnClosureValidator to take the SmallFull object and Errors object. SmallFull object lookup added here as will be used by multiple transaction closure validators rather than each validator call the find. Errors object added as this will be added to by multiple validators
    Update AccountsValidator to call new PreviousPeriodTnClosureValidator
    
    BI-5134

--- src/main/java/uk/gov/companieshouse/api/accounts/validation/AccountsValidator.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/AccountsValidator.java
@@ -1,27 +1,53 @@
 package uk.gov.companieshouse.api.accounts.validation;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.accounts.exception.DataException;
+import uk.gov.companieshouse.api.accounts.links.CompanyAccountLinkType;
+import uk.gov.companieshouse.api.accounts.model.rest.CompanyAccount;
+import uk.gov.companieshouse.api.accounts.model.rest.SmallFull;
 import uk.gov.companieshouse.api.accounts.model.validation.Errors;
+import uk.gov.companieshouse.api.accounts.service.CompanyAccountService;
+import uk.gov.companieshouse.api.accounts.service.impl.SmallFullService;
+import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
 import uk.gov.companieshouse.api.accounts.validation.transactionclosure.CurrentPeriodTnClosureValidator;
+import uk.gov.companieshouse.api.accounts.validation.transactionclosure.PreviousPeriodTnClosureValidator;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class AccountsValidator {
 
     @Autowired
+    private CompanyAccountService companyAccountService;
+
+    @Autowired
+    private SmallFullService smallFullService;
+
+    @Autowired
     private CurrentPeriodTnClosureValidator currentPeriodTnClosureValidator;
 
+    @Autowired
+    private PreviousPeriodTnClosureValidator previousPeriodTnClosureValidator;
+    
     public Errors validationSubmission(Transaction transaction, String companyAccountsId, HttpServletRequest request)
             throws DataException {
 
-        Errors errors;
+        Errors errors = new Errors();
 
-        // Current period validation.
-        errors = currentPeriodTnClosureValidator.isValid(companyAccountsId, request);
+        ResponseObject<CompanyAccount> companyAccountResponseObject = companyAccountService.findById(companyAccountsId, request);
+
+        CompanyAccount companyAccount = companyAccountResponseObject.getData();
+        
+        if(companyAccount.getLinks().get(CompanyAccountLinkType.SMALL_FULL.getLink()) != null) {
+            SmallFull smallFull = smallFullService.find(companyAccountsId, request).getData();
+            
+            // Current period validation.
+            currentPeriodTnClosureValidator.isValid(companyAccountsId, smallFull, request, errors);
+
+            // Previous period validation.
+            previousPeriodTnClosureValidator.isValid(companyAccountsId, smallFull, transaction, request, errors);
+        }
 
         return errors;
     }
--- src/main/java/uk/gov/companieshouse/api/accounts/validation/transactionclosure/CurrentPeriodTnClosureValidator.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/transactionclosure/CurrentPeriodTnClosureValidator.java
@@ -1,30 +1,21 @@
 package uk.gov.companieshouse.api.accounts.validation.transactionclosure;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.accounts.exception.DataException;
-import uk.gov.companieshouse.api.accounts.links.CompanyAccountLinkType;
 import uk.gov.companieshouse.api.accounts.links.SmallFullLinkType;
-import uk.gov.companieshouse.api.accounts.model.rest.CompanyAccount;
 import uk.gov.companieshouse.api.accounts.model.rest.CurrentPeriod;
+import uk.gov.companieshouse.api.accounts.model.rest.SmallFull;
 import uk.gov.companieshouse.api.accounts.model.validation.Errors;
-import uk.gov.companieshouse.api.accounts.service.CompanyAccountService;
 import uk.gov.companieshouse.api.accounts.service.CompanyService;
 import uk.gov.companieshouse.api.accounts.service.impl.CurrentPeriodService;
-import uk.gov.companieshouse.api.accounts.service.impl.SmallFullService;
 import uk.gov.companieshouse.api.accounts.service.response.ResponseObject;
 import uk.gov.companieshouse.api.accounts.service.response.ResponseStatus;
 import uk.gov.companieshouse.api.accounts.validation.BaseValidator;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-
 @Component
 public class CurrentPeriodTnClosureValidator extends BaseValidator {
-
-    private final CompanyAccountService companyAccountService;
-
-    private final SmallFullService smallFullService;
 
     private final CurrentPeriodService currentPeriodService;
 
@@ -33,41 +24,28 @@ public class CurrentPeriodTnClosureValidator extends BaseValidator {
 
     @Autowired
     public CurrentPeriodTnClosureValidator(CompanyService companyService,
-                                           CompanyAccountService companyAccountService,
-                                           SmallFullService smallFullService,
                                            CurrentPeriodService currentPeriodService) {
         super(companyService);
-        this.companyAccountService = companyAccountService;
-        this.smallFullService = smallFullService;
         this.currentPeriodService = currentPeriodService;
     }
 
-    public Errors isValid(String companyAccountsId, HttpServletRequest request) throws DataException {
+    public Errors isValid(String companyAccountsId, SmallFull smallFull, HttpServletRequest request,
+            Errors errors) throws DataException {
 
-        Errors errors = new Errors();
+        if (smallFull.getLinks().get(SmallFullLinkType.CURRENT_PERIOD.getLink()) != null) {
+            ResponseObject<CurrentPeriod> currentPeriodResponseObject =
+                    currentPeriodService.find(companyAccountsId, request);
 
-        Map<String, String> companyAccountLinks;
-        Map<String, String> smallFullLinks;
-
-        ResponseObject<CompanyAccount> companyAccountResponseObject = companyAccountService.findById(companyAccountsId, request);
-        companyAccountLinks = companyAccountResponseObject.getData().getLinks();
-
-        if (companyAccountLinks.get(CompanyAccountLinkType.SMALL_FULL.getLink()) != null) {
-            smallFullLinks = smallFullService.find(companyAccountsId, request).getData().getLinks();
-
-            if (smallFullLinks.get(SmallFullLinkType.CURRENT_PERIOD.getLink()) != null) {
-                ResponseObject<CurrentPeriod> currentPeriodResponseObject = currentPeriodService.find(companyAccountsId, request);
-
-                if (currentPeriodResponseObject.getStatus().equals(ResponseStatus.NOT_FOUND)) {
-                    addError(errors, mandatoryElementMissing, SMALL_FULL_CURRENT_PERIOD_PATH);
-                } else if (currentPeriodResponseObject.getData().getBalanceSheet() == null) {
-                    addError(errors, mandatoryElementMissing, SMALL_FULL_CURRENT_PERIOD_BALANCE_SHEET_PATH);
-                }
-            } else {
+            if (currentPeriodResponseObject.getStatus().equals(ResponseStatus.NOT_FOUND)) {
                 addError(errors, mandatoryElementMissing, SMALL_FULL_CURRENT_PERIOD_PATH);
+            } else if (currentPeriodResponseObject.getData().getBalanceSheet() == null) {
+                addError(errors, mandatoryElementMissing,
+                        SMALL_FULL_CURRENT_PERIOD_BALANCE_SHEET_PATH);
             }
+        } else {
+            addError(errors, mandatoryElementMissing, SMALL_FULL_CURRENT_PERIOD_PATH);
         }
-
+        
         return errors;
     }
 }
