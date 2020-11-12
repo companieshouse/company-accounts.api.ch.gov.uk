8d13cf140cab33fd0d9e2f0f59c3d120b8d50bf1
amoriartyCH <amoriarty@companieshouse.gov.uk>
2020-11-12 09:03:48 +0000

    remove unused imports

--- src/main/java/uk/gov/companieshouse/api/accounts/transformer/RelatedPartyTransactionsTransformer.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/transformer/RelatedPartyTransactionsTransformer.java
@@ -2,11 +2,8 @@ package uk.gov.companieshouse.api.accounts.transformer;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
-import uk.gov.companieshouse.api.accounts.model.entity.smallfull.notes.loanstodirectors.LoansToDirectorsDataEntity;
-import uk.gov.companieshouse.api.accounts.model.entity.smallfull.notes.loanstodirectors.LoansToDirectorsEntity;
 import uk.gov.companieshouse.api.accounts.model.entity.smallfull.notes.relatedpartytransactions.RelatedPartyTransactionsDataEntity;
 import uk.gov.companieshouse.api.accounts.model.entity.smallfull.notes.relatedpartytransactions.RelatedPartyTransactionsEntity;
-import uk.gov.companieshouse.api.accounts.model.rest.smallfull.notes.loanstodirectors.LoansToDirectors;
 import uk.gov.companieshouse.api.accounts.model.rest.smallfull.notes.relatedpartytransactions.RelatedPartyTransactions;
 
 @Component
