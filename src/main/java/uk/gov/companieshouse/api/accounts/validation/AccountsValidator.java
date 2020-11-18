4c946bdc776f9347b70a7b6969d765d32df6a3fb
amoriartyCH <amoriarty@companieshouse.gov.uk>
2020-11-18 13:50:52 +0000

    remove useless comments from validator class

--- src/main/java/uk/gov/companieshouse/api/accounts/validation/AccountsValidator.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/AccountsValidator.java
@@ -88,10 +88,6 @@ public class AccountsValidator extends BaseValidator {
             errors = stocksTxnClosureValidator
                     .validate(companyAccountsId, smallFull, transaction, request, errors, currentPeriodBalanceSheet, previousPeriodBalanceSheet);
 
-            // Debtors
-
-            // .......
-
         } else {
             addError(errors, mandatoryElementMissing, SMALL_FULL_PATH);
         }
