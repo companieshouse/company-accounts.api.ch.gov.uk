ba675b520aad311950f84ef28244134be93bbd03
Faisal Amin <famin@companieshouse.gov.uk>
2020-12-02 13:40:32 +0000

    Add validation for RPT transactions, transaction type This commit checks whether the value supplied for transaction type field of transactions is one of the two acceptable values. If the value supplied does not match either of the two sentences then it throws invalid data error
    Resolves: BI-6264

--- src/main/java/uk/gov/companieshouse/api/accounts/validation/RptTransactionValidator.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/validation/RptTransactionValidator.java
@@ -12,6 +12,8 @@ import uk.gov.companieshouse.api.model.transaction.Transaction;
 public class RptTransactionValidator extends BaseValidator {
 
     private static final String TRANSACTION_BREAKDOWN_PATH_BALANCE_AT_PERIOD_START = "$.transaction.breakdown.balance_at_period_start";
+    private static final String[] TRANSACTION_TYPE_MESSAGE = {"money given to the company by a related party", "money given to a related party by the company"};
+    private static final String TRANSACTION_TYPE = "$.transaction.transaction_type";
 
     @Autowired
     public RptTransactionValidator(CompanyService companyService) {
@@ -24,11 +26,17 @@ public class RptTransactionValidator extends BaseValidator {
 
         boolean isMultipleYearFiler = getIsMultipleYearFiler(transaction);
 
-        if(!isMultipleYearFiler && rptTransaction.getBreakdown().getBalanceAtPeriodStart() != null ) {
+        if (!isMultipleYearFiler && rptTransaction.getBreakdown().getBalanceAtPeriodStart() != null) {
             addError(errors, unexpectedData, TRANSACTION_BREAKDOWN_PATH_BALANCE_AT_PERIOD_START);
             return errors;
         } else if (isMultipleYearFiler && rptTransaction.getBreakdown().getBalanceAtPeriodStart() == null) {
             addError(errors, mandatoryElementMissing, TRANSACTION_BREAKDOWN_PATH_BALANCE_AT_PERIOD_START);
+            return errors;
+        }
+
+        if (!((rptTransaction.getTransactionType().trim().equalsIgnoreCase(TRANSACTION_TYPE_MESSAGE[0]) ||
+                rptTransaction.getTransactionType().trim().equalsIgnoreCase(TRANSACTION_TYPE_MESSAGE[1])))) {
+            addError(errors, invalidValue, TRANSACTION_TYPE);
             return errors;
         }
 
