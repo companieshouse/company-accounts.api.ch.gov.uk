df38769e31436a9b1529ef56ee317c694f1381ef
Aaron Moriarty <amoriarty@companieshouse.gov.uk>
2021-10-12 08:27:36 +0100

    Add unit tests for changes made to both Mongo config files
    MongoConfigTest implemented, now asserts beans are returned not null.
    
    MongoDbConnectionPoolConfigTest implemented, now asserts defaults work
    as expected.

---
+++ src/test/java/uk/gov/companieshouse/api/accounts/MongoConfigTest.java
@@ -0,0 +1,51 @@
+package uk.gov.companieshouse.api.accounts;
+
+import com.mongodb.MongoClientSettings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MongoConfigTest {
+
+
+    @Mock
+    MongoDatabaseFactory mongoDatabaseFactory;
+
+    @Mock
+    MongoMappingContext context;
+
+    @Mock
+    MongoDbConnectionPoolConfig mongoDbConnectionPoolConfig;
+
+    private MongoConfig mongoConfig;
+
+    @BeforeEach
+    void setup() {
+        mongoConfig = new MongoConfig();
+    }
+
+    @Test
+    @DisplayName("Get the bean for mapping mongo converter")
+    void getBeanForMappingMongoConverter() {
+        MappingMongoConverter bean = mongoConfig.mappingMongoConverter(mongoDatabaseFactory, context);
+        assertNotNull(bean);
+    }
+
+    @Test
+    @DisplayName("Get the bean for mongo client settings")
+    void getBeanForMongoClientSettings() {
+        MongoClientSettings bean = mongoConfig.mongoClientSettings(mongoDbConnectionPoolConfig);
+        assertNotNull(bean);
+    }
+}
