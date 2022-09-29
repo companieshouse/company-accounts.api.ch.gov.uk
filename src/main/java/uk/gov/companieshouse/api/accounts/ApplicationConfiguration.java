58ea9dd943fb769a65506efaeb288a73e33a99fd
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2022-09-29 14:08:55 +0100

    Removed messageDigest bean

--- src/main/java/uk/gov/companieshouse/api/accounts/ApplicationConfiguration.java
+++ src/main/java/uk/gov/companieshouse/api/accounts/ApplicationConfiguration.java
@@ -1,15 +1,9 @@
 package uk.gov.companieshouse.api.accounts;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import org.apache.commons.codec.digest.MessageDigestAlgorithms;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.annotation.RequestScope;
-
 import uk.gov.companieshouse.accountsdates.AccountsDatesHelper;
 import uk.gov.companieshouse.accountsdates.impl.AccountsDatesHelperImpl;
 import uk.gov.companieshouse.api.accounts.interceptor.AuthenticationInterceptor;
@@ -25,12 +19,6 @@ import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
 @Configuration
 @PropertySource("classpath:ValidationMessages.properties")
 public class ApplicationConfiguration {
-
-    @Bean
-    @RequestScope
-    public MessageDigest getMessageDigest() throws NoSuchAlgorithmException {
-        return MessageDigest.getInstance(MessageDigestAlgorithms.SHA_256);
-    }
 
     @Bean
     public CharSetValidation getCharSetValidation() {
