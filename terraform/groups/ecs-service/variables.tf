9f53c7c30a2a21acaa85804e4025727b55efeea9
cryder-ch <cryder@companieshouse.gov.uk>
2024-12-02 08:01:01 +0000

    Edited var files and set env files to true

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -120,7 +120,7 @@ variable "ssm_version_prefix" {
 
 variable "use_set_environment_files" {
   type        = bool
-  default     = false
+  default     = true
   description = "Toggle default global and shared  environment files"
 }
 
