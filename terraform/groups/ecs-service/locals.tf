5b1800320828cbbb71b2b993a3ca07949c7983ad
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2025-03-03 14:24:19 +0000

    Update ALB rule priority

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -9,7 +9,7 @@ locals {
   eric_port                  = "10000"
   docker_repo                = "company-accounts.api.ch.gov.uk"
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
-  lb_listener_rule_priority  = 58
+  lb_listener_rule_priority  = 21
   lb_listener_paths          = ["/transactions/*/company-accounts*","/private/transactions/*/company-accounts*","/company-accounts/healthcheck"]
   healthcheck_path           = "/company-accounts/healthcheck" #healthcheck path for company accounts api
   healthcheck_matcher        = "200"
